a7736c517f8e85700dac4223fead692b31e2ef71
ultimate <ultimatej@gmx.net>
2012-09-05 09:05:22 +0200

    added politics DataGenerator

---
+++ src/main/java/com/syncnapsis/utils/data/PolititcsDataGenerator.java
@@ -0,0 +1,49 @@
+package com.syncnapsis.utils.data;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.syncnapsis.data.model.base.Authorities;
+import com.syncnapsis.data.model.base.BaseObject;
+import com.syncnapsis.data.model.base.Contact;
+
+public abstract class PolititcsDataGenerator implements DataGenerator, InitializingBean
+{
+	protected transient final Logger	logger	= LoggerFactory.getLogger(getClass());
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	@Override
+	public void afterPropertiesSet()
+	{
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.syncnapsis.utils.data.DataGenerator#generateDummyData(int)
+	 */
+	@Override
+	public void generateDummyData(int amount)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.syncnapsis.utils.data.DataGenerator#generateTestData(java.util.Properties)
+	 */
+	@Override
+	public void generateTestData(Properties testDataProperties)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public abstract <C1 extends BaseObject<?>, C2 extends BaseObject<?>, A extends Authorities> Contact<C1, C2, A> createContact(C1 contact1, C2 contact2,
+			Map<String, Boolean> authoritiesMap1, Map<String, Boolean> authoritiesMap2);
+}
