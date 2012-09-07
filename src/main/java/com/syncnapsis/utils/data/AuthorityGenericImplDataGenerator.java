ea5ad59ab6c652851378869918434f1e458477fa
ultimate <ultimatej@gmx.net>
2012-09-07 22:24:26 +0200

    updated DataGenerator

--- src/main/java/com/syncnapsis/utils/data/AuthorityGenericImplDataGenerator.java
+++ src/main/java/com/syncnapsis/utils/data/AuthorityGenericImplDataGenerator.java
@@ -25,7 +25,7 @@ public class AuthorityGenericImplDataGenerator extends AuthorityDataGenerator<Au
 	}
 
 	@Override
-	public void afterPropertiesSet()
+	public void afterPropertiesSet() throws Exception
 	{
 		super.afterPropertiesSet();
 		
