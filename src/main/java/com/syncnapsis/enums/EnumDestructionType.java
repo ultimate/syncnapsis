eb513010cd2c5b7c4cd57b985508c5e6e6dcad2f
ultimate <ultimatej@gmx.net>
2012-09-17 19:20:24 +0200

    added EnumDestructionType

---
+++ src/main/java/com/syncnapsis/enums/EnumDestructionType.java
@@ -0,0 +1,8 @@
+package com.syncnapsis.enums;
+
+public enum EnumDestructionType
+{
+	destroyed, // totally destroyed by opponent
+	givenUp, // given up by player itself
+	starved, // population decreased to 0 due to negative growth rate
+}
