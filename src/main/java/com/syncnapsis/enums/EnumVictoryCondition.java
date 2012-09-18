99673ea8e9d1d9295baa6b9d17653546a6026391
ultimate <ultimatej@gmx.net>
2012-09-18 08:55:57 +0200

    added javadoc

--- src/main/java/com/syncnapsis/enums/EnumVictoryCondition.java
+++ src/main/java/com/syncnapsis/enums/EnumVictoryCondition.java
@@ -1,9 +1,24 @@
 package com.syncnapsis.enums;
 
+/**
+ * Enum representing differing victory conditions for matches.<br>
+ * The condition is used to decide when a match is finished and a winner can be determined.
+ * 
+ * @author ultimate
+ */
 public enum EnumVictoryCondition
 {
+	/**
+	 * Rule over xx% of the galaxy for a specified duration
+	 */
 	domination,
-	vendetta,
+	/**
+	 * Destroy all populations/colonies of all other participants
+	 */
 	extermination,
+	/**
+	 * Destroy the populations/colonies of your specified randomly chosen rivals
+	 */
+	vendetta,
 	// some more?
 }
