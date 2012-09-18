3f0788807e706f40a1261a5be9817af5b8bbe7be
ultimate <ultimatej@gmx.net>
2012-09-18 08:55:37 +0200

    updated EnumDestructionType to be suitable for SolarSystemPopulations AND Participants

--- src/main/java/com/syncnapsis/enums/EnumDestructionType.java
+++ src/main/java/com/syncnapsis/enums/EnumDestructionType.java
@@ -1,8 +1,44 @@
 package com.syncnapsis.enums;
 
+import com.syncnapsis.data.model.Match;
+import com.syncnapsis.data.model.Participant;
+import com.syncnapsis.data.model.SolarSystemPopulation;
+
+/**
+ * Enum representing different destruction types for {@link SolarSystemPopulation}s or
+ * {@link Participant}s within a {@link Match}.<br>
+ * <ul>
+ * <li>A population can either be destroyed,
+ * <ul>
+ * <li>by another player (all population is lost)</li>
+ * <li>when a colony is given up (all population is moved away)</li>
+ * <li>when population decreased to 0 due to negative growth rate (no population left)</li>
+ * </ul>
+ * </li>
+ * <li>A participant can either be destroyed,
+ * <ul>
+ * <li>by another player (all populations/systems are lost)</li>
+ * <li>when he/she gives up (player leaves the game)</li>
+ * <li>when the last existing population decreased to 0 due to negative growth rate (no population
+ * left)</li>
+ * </ul>
+ * </li>
+ * </ul>
+ * 
+ * @author ultimate
+ */
 public enum EnumDestructionType
 {
-	destroyed, // totally destroyed by opponent
-	givenUp, // given up by player itself
-	starved, // population decreased to 0 due to negative growth rate
+	/**
+	 * totally destroyed by opponent(all population is lost)
+	 */
+	destroyed,
+	/**
+	 * a colony is given up by the player itself (all population is moved away)
+	 */
+	givenUp,
+	/**
+	 * population decreased to 0 due to negative growth rate (no population left)
+	 */
+	starved,
 }
