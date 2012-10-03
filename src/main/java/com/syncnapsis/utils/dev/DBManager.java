26f4ce223ebe29c0a948505736adc445a7d5f4d0
ultimate <ultimatej@gmx.net>
2012-10-03 14:18:22 +0200

    updated test-tools

--- src/main/java/com/syncnapsis/utils/dev/DBManager.java
+++ src/main/java/com/syncnapsis/utils/dev/DBManager.java
@@ -1,14 +1,24 @@
 package com.syncnapsis.utils.dev;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
+import com.syncnapsis.utils.StringUtil;
+
 public abstract class DBManager
 {
-	protected transient final Logger	logger	= LoggerFactory.getLogger(getClass());
+	public static final String			DEFAULT_PROPERTIES	= "jdbc.properties";
+	
+	protected transient final Logger	logger				= LoggerFactory.getLogger(getClass());
 
 	protected Properties				connectionProperties;
 
@@ -18,8 +28,9 @@ public abstract class DBManager
 	protected String					password;
 	protected String					dialect;
 	protected String					dbName;
+	protected String					sequence;
 
-	public DBManager(Properties connectionProperties, String expectedDialect)
+	public DBManager(Properties connectionProperties, String expectedDialect) throws ClassNotFoundException
 	{
 		super();
 		Assert.notNull(connectionProperties, "connectionProperties must not be null!");
@@ -32,23 +43,178 @@ public abstract class DBManager
 		this.dialect = url.substring(0, url.indexOf("://"));
 		this.dbName = url.substring(url.lastIndexOf("/") + 1);
 
+		this.sequence = connectionProperties.getProperty("sequence", "hibernate_sequence");
+
 		Assert.isTrue(expectedDialect.equals(dialect), "dialect '" + dialect + "' does not match expected '" + expectedDialect + "'");
+
+		Class.forName(driverClassName);
 	}
 
-	public void openConnection()
+	protected Connection openConnection() throws SQLException
 	{
-
-	}
-
-	public void closeConnection()
-	{
-
+		return DriverManager.getConnection(url, username, password);
 	}
 
 	public void dropAll()
 	{
+		try
+		{
+			Connection conn = openConnection();
+			logger.info("dropping database...");
 
+			dropElements(conn, "constraint", getQuery_getTableConstraints(), true);
+			dropElements(conn, "table", getQuery_getTables(), false);
+			dropElements(conn, "sequence", getQuery_getSequences(), false);
+
+			conn.close();
+		}
+		catch(SQLException e)
+		{
+			logger.error("connection error:" + e);
+		}
 	}
+
+	protected boolean dropElements(Connection conn, String identifier, String nameQuery, boolean tableDependent)
+	{
+		try
+		{
+			Statement st = conn.createStatement();
+			Statement stDrop = conn.createStatement();
+			ResultSet rs;
+			String query;
+
+			int elementsDropped = 0;
+			int elementsTotal = 0;
+
+			rs = st.executeQuery(nameQuery);
+			while(rs.next())
+			{
+				elementsTotal++;
+				query = getQuery_drop(identifier, rs.getString(1), tableDependent ? rs.getString(2) : null);
+				stDrop.addBatch(query);
+			}
+			int[] rsDrop = stDrop.executeBatch();
+			for(int i : rsDrop)
+			{
+				if(i != Statement.EXECUTE_FAILED)
+					elementsDropped++;
+			}
+
+			logger.info(StringUtil.fillup(identifier + "s dropped: ", 30, ' ', false)
+					+ StringUtil.fillup(elementsDropped + "/" + elementsTotal, 10, ' ', true));
+
+			if(elementsDropped != elementsTotal)
+				throw new Exception("not all elements dropped!");
+
+			return true;
+		}
+		catch(SQLException e)
+		{
+			logger.error("SQLException occurred", e);
+			return false;
+		}
+		catch(Exception e)
+		{
+			logger.error("Unexpected Exception occurred", e);
+			return false;
+		}
+	}
+
+	public void updateSequence()
+	{
+		try
+		{
+			Connection conn = openConnection();
+
+			logger.info("updating sequence...");
+
+			updateSequence(conn);
+
+			conn.close();
+		}
+		catch(SQLException e)
+		{
+			logger.error("connection error:" + e);
+		}
+	}
+
+	protected boolean updateSequence(Connection conn)
+	{
+		try
+		{
+			Statement st = conn.createStatement();
+			Statement stGet = conn.createStatement();
+			ResultSet rs;
+			ResultSet rsGet;
+			String nameQuery = getQuery_getTables();
+			String query;
+
+			int tablesScanned = 0;
+			int tablesWithId = 0;
+			int max = -1;
+			int curr;
+
+			rs = st.executeQuery(nameQuery);
+			while(rs.next())
+			{
+				tablesScanned++;
+				query = getQuery_getMaxId(rs.getString(1));
+				try
+				{
+					stGet.execute(query);
+					rsGet = stGet.getResultSet();
+					rsGet.next();
+					curr = rsGet.getInt(1);
+					if(curr > max)
+						max = curr;
+					tablesWithId++;
+				}
+				catch(Exception e)
+				{
+					// ignore
+				}
+			}
+			if(max <= 0)
+				max = 1;
+			query = getQuery_setSequenceValue(max);
+			stGet.execute(query);
+			query = getQuery_getSequenceValue();
+			stGet.execute(query);
+			rsGet = stGet.getResultSet();
+			rsGet.next();
+			curr = rsGet.getInt(1);
+
+			logger.info(StringUtil.fillup("tables scanned: ", 30, ' ', false) + StringUtil.fillup("" + tablesScanned, 10, ' ', true));
+			logger.info(StringUtil.fillup("tables with id: ", 30, ' ', false) + StringUtil.fillup("" + tablesWithId, 10, ' ', true));
+			logger.info(StringUtil.fillup("maximum id is: ", 30, ' ', false) + StringUtil.fillup("" + max, 10, ' ', true));
+			logger.info(StringUtil.fillup("sequence set to: ", 30, ' ', false) + StringUtil.fillup("" + curr, 10, ' ', true));
+
+			if(max < 0)
+				throw new Exception("could not update sequence!");
+
+			return true;
+		}
+		catch(SQLException e)
+		{
+			logger.error("SQLException occurred", e);
+			return false;
+		}
+		catch(Exception e)
+		{
+			logger.error("Unexpected Exception occurred", e);
+			return false;
+		}
+	}
+
+	// public void createUser() throws Exception
+	// {
+	//
+	// }
+
+	// public void createDatabase() throws Exception
+	// {
+	//
+	// }
 
 	protected abstract String getQuery_getTables();
 
@@ -56,11 +222,46 @@ public abstract class DBManager
 
 	protected abstract String getQuery_getSequences();
 
-	protected abstract String getQuery_createDatabase(String dbName, String dbOwner);
+	// protected abstract String getQuery_createDatabase(String dbName, String dbOwner);
+	//
+	// protected abstract String getQuery_createUser(String user, String pw);
+	//
+	// protected abstract String getQuery_getUsers();
+	//
+	// protected abstract String getQuery_getDatabases();
 
-	protected abstract String getQuery_createUser(String user, String pw);
+	protected String getQuery_drop(String identifier, String name, String table)
+	{
 
-	protected abstract String getQuery_getUsers();
+		return (table != null ? "alter table " + table + " " : "") + "drop " + identifier + " " + name + ";";
+	}
 
-	protected abstract String getQuery_getDatabases();
+	protected String getQuery_getMaxId(String table)
+	{
+		return "select max(id) from " + table + ";";
+	}
+
+	protected String getQuery_setSequenceValue(int value)
+	{
+		return "select setval('" + sequence + "', " + value + ")";
+	}
+
+	protected String getQuery_getSequenceValue()
+	{
+		return "select nextval('" + sequence + "')";
+	}
+
+	protected static void execute(List<String> argsList, DBManager dbm)
+	{
+		dbm.logger.info(argsList.size() + " operations to perform: " + argsList);
+
+		if(argsList.contains("-drop"))
+		{
+			dbm.dropAll();
+		}
+		if(argsList.contains("-sequence"))
+		{
+			dbm.updateSequence();
+		}
+	}
 }
--- src/main/java/com/syncnapsis/utils/dev/PostgreSQLDBManager.java
+++ src/main/java/com/syncnapsis/utils/dev/PostgreSQLDBManager.java
@@ -1,12 +1,18 @@
 package com.syncnapsis.utils.dev;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import com.syncnapsis.utils.PropertiesUtil;
 
 public class PostgreSQLDBManager extends DBManager
 {
 	public static final String DIALECT = "jdbc:postgresql";
 
-	public PostgreSQLDBManager(Properties connectionProperties)
+	public PostgreSQLDBManager(Properties connectionProperties) throws ClassNotFoundException
 	{
 		super(connectionProperties, DIALECT);
 	}
@@ -29,29 +35,61 @@ public class PostgreSQLDBManager extends DBManager
 		return "SELECT sequence_name FROM information_schema.sequences;";
 	}
 
-	@Override
-	protected String getQuery_createDatabase(String dbName, String dbOwner)
-	{
-		return "CREATE DATABASE \"" + dbName + "\" WITH OWNER=\"" + dbOwner + "\" ENCODING='UTF8' TABLESPACE=pg_default;";
-	}
+//	@Override
+//	protected String getQuery_createDatabase(String dbName, String dbOwner)
+//	{
+//		return "CREATE DATABASE \"" + dbName + "\" WITH OWNER=\"" + dbOwner + "\" ENCODING='UTF8' TABLESPACE=pg_default;";
+//	}
+//
+//	@Override
+//	protected String getQuery_createUser(String user, String pw)
+//	{
+//		return "CREATE ROLE \"" + user + "\" LOGIN ENCRYPTED PASSWORD '" + pw + "' NOSUPERUSER INHERIT CREATEDB CREATEROLE;";
+//	}
+//
+//	@Override
+//	protected String getQuery_getUsers()
+//	{
+//		return "SELECT usename FROM pg_catalogger.pg_user;";
+//	}
+//
+//	@Override
+//	protected String getQuery_getDatabases()
+//	{
+//		return "SELECT datname FROM pg_catalogger.pg_database;";
+//	}
 
-	@Override
-	protected String getQuery_createUser(String user, String pw)
+	public static void main(String[] args)
 	{
-		return "CREATE ROLE \"" + user + "\" LOGIN ENCRYPTED PASSWORD '" + pw + "' NOSUPERUSER INHERIT CREATEDB CREATEROLE;";
+		String properties = DEFAULT_PROPERTIES;
+		
+		// parse args
+		List<String> argsList = new ArrayList<String>(args.length);
+		for(String arg : args)
+		{
+			if(arg.endsWith(".properties"))
+			{
+				properties = arg;
+			}
+			else
+			{
+				argsList.add(arg.toLowerCase());
+			}
+		}
+		
+		try
+		{
+			execute(argsList, new PostgreSQLDBManager(PropertiesUtil.loadProperties(new File(properties))));
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("could not load jdbc-driver-class: " + e.getMessage());
+			e.printStackTrace();
+		}
+		catch(IOException e)
+		{
+			System.out.println("could not read properties-file: " + properties);
+			e.printStackTrace();
+		}
 	}
-
-	@Override
-	protected String getQuery_getUsers()
-	{
-		return "SELECT usename FROM pg_catalogger.pg_user;";
-	}
-
-	@Override
-	protected String getQuery_getDatabases()
-	{
-		return "SELECT datname FROM pg_catalogger.pg_database;";
-	}
-
-	// ("updateUSER", "UPDATE pg_authid SET rolcatupdate=true WHERE OID=34605::oid;");
 }
