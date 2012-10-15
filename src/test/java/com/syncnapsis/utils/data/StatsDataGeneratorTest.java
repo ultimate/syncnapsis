a2fc6ae9b298c59e8353dda3817a1556eda02c0e
ultimate <ultimatej@gmx.net>
2012-10-15 19:26:32 +0200

    added dummy StatsDataGeneratorTest

---
+++ src/test/java/com/syncnapsis/utils/data/StatsDataGeneratorTest.java
@@ -0,0 +1,11 @@
+package com.syncnapsis.utils.data;
+
+import com.syncnapsis.tests.BaseSpringContextTestCase;
+
+public class StatsDataGeneratorTest extends BaseSpringContextTestCase
+{
+	public void testDataGenerator() throws Exception
+	{
+		logger.info("nothing to test here yet: generator is abstract!");
+	}
+}
