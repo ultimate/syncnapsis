dd41fb8eee9fe579d6eae2db36a189f77cbcd267
ultimate <ultimatej@gmx.net>
2012-11-12 17:11:07 +0100

    started index.html design: ad-area

---
+++ src/main/webapp/scripts/ad.js
@@ -0,0 +1,68 @@
+shrinkAd = function()
+{
+	// reset height
+	document.getElementById("ad_left_img").style.height = "";
+	document.getElementById("ad_right_img").style.height = "";
+
+	// get target heights
+	var available = document.getElementById("ad_left").offsetHeight;
+	var original = document.getElementById("ad_left_img").height;
+
+	// get the minimum
+	var height = Math.min(available, original);
+
+	// set the new height
+	document.getElementById("ad_left_img").style.height = height + "px";
+	document.getElementById("ad_right_img").style.height = height + "px";
+};
+
+Events.addEventListener(Events.ONRESIZE, shrinkAd, window);
+
+var AD_TOGGLE_DIR = 0;
+var AD_TOGGLE_STEP = 10;
+var AD_TOGGLE_DELAY = 10;
+var AD_SHOW_INTERVAL = 300000;
+var AD_SHOW_DURATION = 10000;
+var AD_SHOW_USER = true;
+
+toggleAd = function(user)
+{
+	if(layout_horizontal.elementSizes[0] == 0)
+	{
+		AD_TOGGLE_DIR = AD_TOGGLE_STEP;
+		if(user)
+			AD_SHOW_USER = true;
+	}
+	else if(layout_horizontal.elementSizes[0] == AD_WIDTH)
+	{
+		AD_TOGGLE_DIR = -AD_TOGGLE_STEP;
+		if(user)
+			AD_SHOW_USER = false;
+	}
+	layout_horizontal.elementSizes[0] += AD_TOGGLE_DIR;
+	layout_horizontal.elementSizes[2] += AD_TOGGLE_DIR;
+	layout_horizontal.fill();
+	layout_ad_left.fill();
+	layout_ad_right.fill();
+	if(layout_horizontal.elementSizes[0] == 0)
+		AD_TOGGLE_DIR = 0;
+	else if(layout_horizontal.elementSizes[0] == AD_WIDTH)
+		AD_TOGGLE_DIR = 0;
+	if(AD_TOGGLE_DIR != 0)
+		setTimeout(toggleAd, AD_TOGGLE_DELAY);
+};
+
+showAd = function()
+{
+	if(layout_horizontal.elementSizes[0] == 0)
+		toggleAd(false);
+	setTimeout(hideAd, AD_SHOW_DURATION);
+};
+
+hideAd = function()
+{
+	if(!AD_SHOW_USER && layout_horizontal.elementSizes[0] == AD_WIDTH)
+		toggleAd(false);
+};
+
+setInterval(showAd, AD_SHOW_INTERVAL);
--- src/main/webapp/scripts/styles.js
+++ src/main/webapp/scripts/styles.js
@@ -12,19 +12,27 @@ Styles.layout = {};
 Styles.layout.HORIZONTAL = 1;
 Styles.layout.VERTICAL = 2;
 
-FillLayout = function(elements, elementSizes, direction)
+window[Styles.constant.WIDTH] = window.innerWidth;
+window[Styles.constant.HEIGHT] = window.innerHeight;
+
+Styles.FillLayout = function(elements, elementSizes, direction)
 {
 	if(elements.length != elementSizes.length)
 		throw new Error("elements and elementSizes must have same length!");
 	this.elements = new Array();
+	this.elementParent = null;
 	for(var i = 0; i < elements.length; i++)
 	{
 		if(elements[i] == null)
 			throw new Error("elementIds must not contain null values!");
-		if(typeof(elements) == "string")
-			this.elements[i] = document.getElementyById(elements[i]);
+		if(typeof(elements[i]) == Reflections.type.STRING)
+			this.elements[i] = document.getElementById(elements[i]);
 		else 
 			this.elements[i] = elements[i];
+		if(this.elementParent == null)
+			this.elementParent = this.elements[i].parentNode;
+		else if(this.elements[i].parentNode != this.elementParent)
+			throw new Error("elements must have same parent!");
 	}
 	this.elementSizes = elementSizes;
 	this.nullSizes = 0;
@@ -33,41 +41,53 @@ FillLayout = function(elements, elementSizes, direction)
 		if(elementSizes[i] == null)
 			this.nullSizes++;
 	}
-	if(direction != Layout.layout.HORIZONTAL && direction != Layout.layout.VERTICAL)
+	if(direction != Styles.layout.HORIZONTAL && direction != Styles.layout.VERTICAL)
 		throw new Error("Illegal direction '" + direction + "'!");
 	this.direction = direction;
 	
-	// TODO attach event to resize
+	this._handlerObject = this;
+	this._handlerMethod = this.fill;
+	Events.addEventListener(Events.ONRESIZE, this.anonymous() , window);
 }
 
-FillLayout.fill = function()
+Styles.FillLayout.prototype = new Events.EventHandler();
+
+Styles.FillLayout.prototype.fill = function()
 {
-	var coordinateTarget = window[this.direction];
+	var coordinateTarget;
+	if(this.direction == Styles.layout.HORIZONTAL)
+		coordinateTarget = this.elementParent.offsetWidth;
+	else
+		coordinateTarget = this.elementParent.offsetHeight;
 	var coordinateTotal = 0;
 	var coordinateSum = 0;
-	for(var i = 0; i < element.length; i++)
+	for(var i = 0; i < this.elements.length; i++)
 	{
 		if(this.elementSizes[i] != null)
 			coordinateTotal += this.elementSizes[i];
 	}
-	var nullSize = (coordinateTarget - coordinateTotal) / nullSizes;
-	for(var i = 0; i < element.length; i++)
+	var nullSize = (coordinateTarget - coordinateTotal) / this.nullSizes;
+	var size;
+	for(var i = 0; i < this.elements.length; i++)
 	{
+		if(this.elementSizes[i] != null)
+			size = this.elementSizes[i];
+		else
+			size = nullSize;
+		
 		if(this.direction == Styles.layout.HORIZONTAL)
 		{
-			this.elements[i].style.left = coordinateSum + "px";
-			if(this.elementSizes[i] != null)
-				this.elements[i].style.width = this.elementSizes[i] + "px";
-			else
-				this.elements[i].style.width = nullSize + "px";
+			this.elements[i].style.left = coordinateSum + "px";			
+			this.elements[i].style.width = size + "px";
+			//this.elements[i].style.height = this.elementParent.offsetHeight + "px";
+			coordinateSum += size;			
 		}
 		else
 		{
 			this.elements[i].style.top = coordinateSum + "px";
-			if(this.elementSizes[i] != null)
-				this.elements[i].style.height = this.elementSizes[i] + "px";
-			else
-				this.elements[i].style.height = nullSize + "px";
+			this.elements[i].style.height = size + "px";
+			//this.elements[i].style.width = this.elementParent.offsetWidth + "px";
+			coordinateSum += size;
 		}
 	}
 };
