2c895ccacb5324cce1dd811f40b0bb406557d982
ultimate <ultimatej@gmx.net>
2012-12-24 12:58:25 +0100

    added copyright info

--- src/main/webapp/old/gui/jstemplates/windowTable.js
+++ src/main/webapp/old/gui/jstemplates/windowTable.js
@@ -1,3 +1,17 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
 var windowTable = '<table width="100%" height="100%" border=0 cellspacing=0 cellpadding=0>' +
 						 '<tr bgcolor="#000000" height={outerFrameSize}>' +
 						 	'<td id="{windowName}.resize1" width={outerFrameSize} style="cursor: nw-resize; background-image:url(images/skins/{skinname}/window/window_northwest.png)" direction="nw"><img src="images/spacer.gif"></td>' +
