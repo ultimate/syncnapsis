8dd18e0d6ded0958aa379000a6afa47e44d5eae0
ultimate <ultimatej@gmx.net>
2013-01-01 18:20:58 +0100

    added support for include (additional to forward)

--- src/main/java/com/syncnapsis/web/URLRewriteFilter.java
+++ src/main/java/com/syncnapsis/web/URLRewriteFilter.java
@@ -1,11 +1,14 @@
 /**
  * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
  * This program is free software; you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation; either version
  * 3 of the License, or any later version.
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Plublic License along with this program;
  * if not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,6 +33,34 @@ import com.syncnapsis.websockets.engine.FilterEngine;
  */
 public abstract class URLRewriteFilter extends FilterEngine
 {
+	/**
+	 * Use {@link RequestDispatcher#include(ServletRequest, ServletResponse)} instead of
+	 * {@link RequestDispatcher#forward(ServletRequest, ServletResponse)}
+	 */
+	private boolean	useInclude	= false;
+
+	/**
+	 * Use {@link RequestDispatcher#include(ServletRequest, ServletResponse)} instead of
+	 * {@link RequestDispatcher#forward(ServletRequest, ServletResponse)}
+	 * 
+	 * @return true or false
+	 */
+	public boolean isUseInclude()
+	{
+		return useInclude;
+	}
+
+	/**
+	 * Use {@link RequestDispatcher#include(ServletRequest, ServletResponse)} instead of
+	 * {@link RequestDispatcher#forward(ServletRequest, ServletResponse)}
+	 * 
+	 * @param useInclude - true or false
+	 */
+	public void setUseInclude(boolean useInclude)
+	{
+		this.useInclude = useInclude;
+	}
+
 	/**
 	 * Empty Default Constructor
 	 */
@@ -73,10 +104,19 @@ public abstract class URLRewriteFilter extends FilterEngine
 		if(newURL != null)
 		{
 			RequestDispatcher dispatcher = httpRequest.getRequestDispatcher(newURL);
-			dispatcher.forward(httpRequest, httpResponse);
+			
+			if(!useInclude)
+			{
+				dispatcher.forward(httpRequest, httpResponse);
+			}
+			else
+			{
+				dispatcher.include(httpRequest, httpResponse);
+			}
 		}
 		else
 		{
+			logger.debug("continuing filter chain...");
 			chain.doFilter(httpRequest, httpResponse);
 		}
 	}
