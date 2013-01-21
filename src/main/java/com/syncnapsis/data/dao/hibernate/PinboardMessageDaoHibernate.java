c3dc56cba4a7e46738d3d134c1cc0209d255be13
ultimate <ultimatej@gmx.net>
2013-01-21 20:24:41 +0100

    added Pinboard & PinboardMessage Entities (including DAO, Manager, app-ctx, hibernate.cfg.xml, test-data)

---
+++ src/main/java/com/syncnapsis/data/dao/hibernate/PinboardMessageDaoHibernate.java
@@ -0,0 +1,35 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.syncnapsis.data.dao.hibernate;
+
+import com.syncnapsis.data.dao.PinboardMessageDao;
+import com.syncnapsis.data.model.PinboardMessage;
+
+/**
+ * Dao-Implementation for Hibernate for access to PinboardMessage
+ * 
+ * @author ultimate
+ */
+public class PinboardMessageDaoHibernate extends GenericDaoHibernate<PinboardMessage, Long> implements PinboardMessageDao
+{
+	/**
+	 * Create a new DAO-Instance using the super-class GenericDaoHibernate
+	 * with the model-Class PinboardMessage
+	 */
+	public PinboardMessageDaoHibernate()
+	{
+		super(PinboardMessage.class);
+	}
+}
