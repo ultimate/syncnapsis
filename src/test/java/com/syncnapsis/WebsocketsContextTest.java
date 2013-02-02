da77fdb01fe7bb3e303212824109c847f72b742c
ultimate <ultimatej@gmx.net>
2013-02-02 12:49:10 +0100

    refactored application-context: moved some beans to appropriated project and file added Websockets-Base-Tests

---
+++ src/test/java/com/syncnapsis/WebsocketsContextTest.java
@@ -0,0 +1,23 @@
+package com.syncnapsis;
+
+
+import com.syncnapsis.providers.TimeProvider;
+import com.syncnapsis.tests.BaseSpringContextTestCase;
+import com.syncnapsis.utils.ApplicationContextUtil;
+
+public class WebsocketsContextTest extends BaseSpringContextTestCase
+{
+	private ApplicationContextUtil applicationContextUtil;
+	private TimeProvider timeProvider;
+	
+	public void testContextConfiguration()
+	{
+		assertNotNull(applicationContext);
+		
+		assertNotNull(applicationContextUtil);
+		
+		assertNotNull(timeProvider);
+		
+		assertNotNull(ApplicationContextUtil.getBean("timeProvider"));
+	}
+}
