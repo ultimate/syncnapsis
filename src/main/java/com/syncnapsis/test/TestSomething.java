47fe69d572bde0872d6449ee071198614a5e2c53
ultimate <ultimatej@gmx.net>
2013-03-11 21:33:44 +0100

    Tested something

--- src/main/java/com/syncnapsis/test/TestSomething.java
+++ src/main/java/com/syncnapsis/test/TestSomething.java
@@ -1,11 +1,14 @@
 /**
  * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
  * This program is free software; you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation; either version
  * 3 of the License, or any later version.
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Plublic License along with this program;
  * if not, see <http://www.gnu.org/licenses/>.
  */
@@ -15,13 +18,9 @@ import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.hibernate.metadata.ClassMetadata;
-
 import com.syncnapsis.data.model.Player;
-import com.syncnapsis.data.model.base.BaseObject;
 import com.syncnapsis.data.model.base.Identifiable;
 import com.syncnapsis.tests.annotations.Untested;
-import com.syncnapsis.utils.HibernateUtil;
 import com.syncnapsis.utils.ReflectionsUtil;
 
 @Untested
@@ -29,6 +28,12 @@ public class TestSomething
 {
 	public static void main(String[] args) throws Exception
 	{	
+		for(int i = 0; i < 20; i++)
+			System.out.println((i*5)%7+1);
+		
+		for(byte b = Byte.MIN_VALUE; b < Byte.MAX_VALUE; b++)
+			System.out.println((b+256) % 256);
+		
 		ReflectionsUtil.getActualTypeArguments(Player.class, Identifiable.class); // warm up
 		long start = System.nanoTime();
 		Type[] t = ReflectionsUtil.getActualTypeArguments(Player.class, Identifiable.class);
