e060e85ee53c525344a7b9ababad429582e32b44
ultimate <ultimatej@gmx.net>
2013-03-15 21:33:54 +0100

    Added: Validator and some implementations

---
+++ src/test/java/com/syncnapsis/security/validation/BlackListValidatorTest.java
@@ -0,0 +1,41 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.syncnapsis.security.validation;
+
+import java.util.Arrays;
+
+import com.syncnapsis.tests.LoggerTestCase;
+import com.syncnapsis.tests.annotations.TestExcludesMethods;
+
+/**
+ * @author ultimate
+ * 
+ */
+@TestExcludesMethods({ "*etBlackList" })
+public class BlackListValidatorTest extends LoggerTestCase
+{
+	public void testIsValid() throws Exception
+	{
+		BlackListValidator<String> v = new BlackListValidator<String>();
+		v.setBlackList(Arrays.asList(new String[] {
+				"badword",
+				"verybadword"
+		}));
+
+		assertFalse(v.isValid("badword"));
+		assertFalse(v.isValid("verybadword"));
+		assertTrue(v.isValid("goodword"));
+	}
+}
