4439c2bfe71fa0bd9365b0c2c1c6e7deb9a1d8cf
ultimate <ultimatej@gmx.net>
2013-04-03 20:33:06 +0200

    Added: BaseApplicationMailer for standard application related mails Added: mail.properties for tests

---
+++ src/main/java/com/syncnapsis/utils/mail/BaseApplicationMailer.java
@@ -0,0 +1,271 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.syncnapsis.utils.mail;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.mail.internet.AddressException;
+
+import com.syncnapsis.data.model.Action;
+import com.syncnapsis.data.model.User;
+import com.syncnapsis.data.model.UserRole;
+import com.syncnapsis.utils.MessageUtil;
+
+/**
+ * TemplateMailer extension offering support for sending standard application mails like
+ * registration or email verifications etc.<br>
+ * All remplates required must be provided with the mail.properties. The keys for those templates
+ * are defined as constants within the class and are listed in
+ * {@link BaseApplicationMailer#REQUIRED_TEMPLATES}:
+ * <ul>
+ * <li>{@link BaseApplicationMailer#TEMPLATE_REGISTRATION_VERIFY}: "registration.verify"</li>
+ * <li>{@link BaseApplicationMailer#TEMPLATE_MAIL_ADDRESS_VERIFY}: "mail.verify"</li>
+ * <li>{@link BaseApplicationMailer#TEMPLATE_NOTIFICATION}: "notification"</li>
+ * <li>{@link BaseApplicationMailer#TEMPLATE_USERROLE_CHANGED}: "userrole.changed"</li>
+ * </ul>
+ * 
+ * @author ultimate
+ */
+public class BaseApplicationMailer extends TemplateMailer
+{
+	/**
+	 * Template name for {@link BaseApplicationMailer#sendVerifyRegistration(User)}
+	 */
+	public static final String		TEMPLATE_REGISTRATION_VERIFY	= "registration.verify";
+	/**
+	 * Template name for {@link BaseApplicationMailer#sendVerifyMailAddress(User)}
+	 */
+	public static final String		TEMPLATE_MAIL_ADDRESS_VERIFY	= "mail.verify";
+	/**
+	 * Template name for {@link BaseApplicationMailer#sendGeneralNotification(List)}
+	 */
+	public static final String		TEMPLATE_NOTIFICATION			= "notification";
+	/**
+	 * Template name for {@link BaseApplicationMailer#sendUserRoleChangedNotification(User)}
+	 */
+	public static final String		TEMPLATE_USERROLE_CHANGED		= "userrole.changed";
+	/**
+	 * An Array containing the names of all required templates.
+	 */
+	public static final String[]	REQUIRED_TEMPLATES				= new String[] { TEMPLATE_REGISTRATION_VERIFY, TEMPLATE_MAIL_ADDRESS_VERIFY,
+			TEMPLATE_NOTIFICATION, TEMPLATE_USERROLE_CHANGED,		};
+
+	/**
+	 * Construct a new Mailer by loading the Properties from the given file
+	 * 
+	 * @param propertiesFile - the properties-file
+	 * @throws IOException if loading the properties fails
+	 */
+	public BaseApplicationMailer(File propertiesFile) throws IOException
+	{
+		super(propertiesFile);
+		checkTemplates();
+	}
+
+	/**
+	 * Construct a new Mailer with the given properties
+	 * 
+	 * @param properties - the Properties
+	 */
+	public BaseApplicationMailer(Properties properties)
+	{
+		super(properties);
+		checkTemplates();
+	}
+
+	/**
+	 * Check the template configuration if all required templates are present.<br>
+	 * Since this method is used in the constructors it will throw an IllegalArgumentException for
+	 * the first required template not found and abort any further check.
+	 * 
+	 * @throws IllegalArgumentException if a required template is not found.
+	 */
+	protected void checkTemplates()
+	{
+		Set<String> templates = getTemplateNames();
+		for(String req : REQUIRED_TEMPLATES)
+		{
+			if(!templates.contains(req))
+				throw new IllegalArgumentException("required template '" + req + "' not found");
+		}
+	}
+
+	/**
+	 * Send a registration verification with a request for the user to validate his registration.<br>
+	 * <br>
+	 * The exact content will depend on the template defined in the mail.properties but support for
+	 * including {@link User} and {@link Action} properties is given by default. The Action is
+	 * representing the action that the newly registered user has to execute (via clicking a link)
+	 * for activating his account.<br>
+	 * <br>
+	 * This method will do a generic lookup for all keys used in the template using
+	 * {@link MessageUtil#extractValues(List, Object...)}.
+	 * 
+	 * @see MessageUtil#extractValues(List, Object...)
+	 * @see MessageUtil#getUsedTemplateKeys(String)
+	 * @see TemplateMailer#send(String, Map, String)
+	 * @param user - the newly registered user
+	 * @param action - the action for confirming registration
+	 * @return true if a message has been send, false otherwise
+	 */
+	public boolean sendVerifyRegistration(User user, Action action)
+	{
+		String template = TEMPLATE_REGISTRATION_VERIFY;
+		List<String> keys = MessageUtil.getUsedTemplateKeys(getText(template));
+		keys.addAll(MessageUtil.getUsedTemplateKeys(getSubject(template)));
+		Map<String, Object> values = MessageUtil.extractValues(keys, user, action);
+		// fill other values?
+		try
+		{
+			return send(template, values, user.getEmail());
+		}
+		catch(AddressException e)
+		{
+			logger.error("could not send mail to '" + e.getRef() + "'");
+			return false;
+		}
+	}
+
+	/**
+	 * Send a email verification with the request for the user to validate his email.<br>
+	 * <br>
+	 * The exact content will depend on the template defined in the mail.properties but support for
+	 * including {@link User} and {@link Action} properties is given by default. The Action is
+	 * representing the action that the registered user has to execute (via clicking a link)
+	 * to validate his newly entered email address.<br>
+	 * <br>
+	 * This method will do a generic lookup for all keys used in the template using
+	 * {@link MessageUtil#extractValues(List, Object...)}.
+	 * 
+	 * @see MessageUtil#extractValues(List, Object...)
+	 * @see MessageUtil#getUsedTemplateKeys(String)
+	 * @see TemplateMailer#send(String, Map, String)
+	 * @param user - the user that changed his email address
+	 * @param action - the action for confirming the email address change
+	 * @return true if a message has been send, false otherwise
+	 */
+	public boolean sendVerifyMailAddress(User user, Action action)
+	{
+		String template = TEMPLATE_MAIL_ADDRESS_VERIFY;
+		List<String> keys = MessageUtil.getUsedTemplateKeys(getText(template));
+		keys.addAll(MessageUtil.getUsedTemplateKeys(getSubject(template)));
+		Map<String, Object> values = MessageUtil.extractValues(keys, user, action);
+		// fill other values?
+		try
+		{
+			return send(template, values, user.getEmail());
+		}
+		catch(AddressException e)
+		{
+			logger.error("could not send mail to '" + e.getRef() + "'");
+			return false;
+		}
+	}
+
+	/**
+	 * Send a notification email if a user's role has changed.<br>
+	 * <br>
+	 * The exact content will depend on the template defined in the mail.properties but support for
+	 * including {@link User} (and therefore {@link UserRole}) properties is given by default.
+	 * Normally the template should include information about the role change like
+	 * <ul>
+	 * <li>the old role</li>
+	 * <li>the new role</li>
+	 * <li>the role expire date</li>
+	 * </ul>
+	 * <br>
+	 * This method will do a generic lookup for all keys used in the template using
+	 * {@link MessageUtil#extractValues(List, Object...)}.
+	 * 
+	 * @see MessageUtil#extractValues(List, Object...)
+	 * @see MessageUtil#getUsedTemplateKeys(String)
+	 * @see TemplateMailer#send(String, Map, String)
+	 * @param user - the user who's role has changed (including the new role)
+	 * @param oldRole - the old role of the user
+	 * @param reason - the reason for the role change
+	 * @return true if a message has been send, false otherwise
+	 */
+	public boolean sendUserRoleChangedNotification(User user, UserRole oldRole, String reason)
+	{
+		String template = TEMPLATE_USERROLE_CHANGED;
+		List<String> keys = MessageUtil.getUsedTemplateKeys(getText(template));
+		keys.addAll(MessageUtil.getUsedTemplateKeys(getSubject(template)));
+		Map<String, Object> values = MessageUtil.extractValues(keys, user, oldRole);
+		values.put("reason", reason);
+		// fill other values?
+		try
+		{
+			return send(template, values, user.getEmail());
+		}
+		catch(AddressException e)
+		{
+			logger.error("could not send mail to '" + e.getRef() + "'");
+			return false;
+		}
+	}
+
+	/**
+	 * Send a notification email to all users given.<br>
+	 * <br>
+	 * The template usually only will define the general layout of the notification message and the
+	 * real content will be defined by the parameters given. Nevertheless support for
+	 * including {@link User} properties is given by default.<br>
+	 * <br>
+	 * This method will do a generic lookup for all keys used in the template using
+	 * {@link MessageUtil#extractValues(List, Object...)} for each user to create individual
+	 * messages. Additionally the mail will be sent to each user separately to avoid visibility of
+	 * other recipients.
+	 * 
+	 * @see MessageUtil#extractValues(List, Object...)
+	 * @see MessageUtil#getUsedTemplateKeys(String)
+	 * @see TemplateMailer#send(String, Map, String)
+	 * @param users - the list of users to send the message to
+	 * @param notificationSubject - the subject to include into the message as {subject}
+	 * @param notificationText - the text to include into the message as {text}
+	 * @return true if a message has been send, false otherwise
+	 */
+	public int sendGeneralNotification(List<User> users, String notificationSubject, String notificationText)
+	{
+		String template = TEMPLATE_NOTIFICATION;
+		List<String> keys = MessageUtil.getUsedTemplateKeys(getText(template));
+		keys.addAll(MessageUtil.getUsedTemplateKeys(getSubject(template)));
+		keys.addAll(MessageUtil.getUsedTemplateKeys(notificationSubject));
+		keys.addAll(MessageUtil.getUsedTemplateKeys(notificationText));
+		Map<String, Object> values;
+		int messagesSent = 0;
+		for(User user : users)
+		{
+			try
+			{
+				values = MessageUtil.extractValues(keys, user);
+				values.put("subject", notificationSubject);
+				values.put("text", notificationText);
+				// fill other values?
+				if(send(template, values, user.getEmail()))
+					messagesSent++;
+			}
+			catch(AddressException e)
+			{
+				logger.error("could not send mail to '" + e.getRef() + "'");
+			}
+		}
+		return messagesSent;
+	}
+}
---
+++ src/test/java/com/syncnapsis/utils/mail/BaseApplicationMailerTest.java
@@ -0,0 +1,223 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.syncnapsis.utils.mail;
+
+import java.io.File;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.subethamail.wiser.Wiser;
+
+import com.syncnapsis.data.model.Action;
+import com.syncnapsis.data.model.User;
+import com.syncnapsis.data.model.UserRole;
+import com.syncnapsis.tests.LoggerTestCase;
+import com.syncnapsis.tests.annotations.TestExcludesMethods;
+
+/**
+ * @author ultimate
+ */
+@TestExcludesMethods("checkTemplates")
+public class BaseApplicationMailerTest extends LoggerTestCase
+{
+	public void testSendVerifyRegistration() throws Exception
+	{
+		BaseApplicationMailer m = new BaseApplicationMailer(new File("target/test-classes/mail.properties"));
+
+		User user = new User();
+		user.setUsername("new guy");
+		user.setEmail("newguy@example.com");
+
+		Action action = new Action();
+		action.setCode("34ae8c3f0142aa3bc4");
+
+		String subjectExpected = "Welcome to syncnapsis, " + user.getUsername();
+		String textExpected = "Hello " + user.getUsername() + ",\n" + "thank you for your registration and welcome to syncnapsis.\n"
+				+ "Please confirm your registration by clicking the following link:\n" + "http://www.syncnapsis.com/test/activate/"
+				+ action.getCode() + "\n" + "Best regards";
+		textExpected = textExpected.replace("\n", "\r\n"); // mailing replaced the line endings.
+
+		String toExpected = user.getEmail();
+
+		Wiser w = new Wiser();
+		w.start();
+
+		try
+		{
+			assertTrue(m.sendVerifyRegistration(user, action));
+
+			assertEquals(1, w.getMessages().size());
+			MimeMessage msg = w.getMessages().get(0).getMimeMessage();
+
+			assertEquals(subjectExpected, msg.getSubject());
+			assertEquals(textExpected, msg.getContent());
+			assertEquals(1, msg.getAllRecipients().length);
+			assertEquals(1, msg.getRecipients(RecipientType.TO).length);
+			assertEquals(toExpected, ((InternetAddress) msg.getRecipients(RecipientType.TO)[0]).getAddress());
+			assertEquals(1, msg.getFrom().length);
+			assertEquals(m.getGlobalProperty(Mailer.KEY_FROM), ((InternetAddress) msg.getFrom()[0]).getAddress());
+		}
+		finally
+		{
+			w.stop();
+		}
+	}
+
+	public void testSendVerifyMailAddress() throws Exception
+	{
+		BaseApplicationMailer m = new BaseApplicationMailer(new File("target/test-classes/mail.properties"));
+
+		User user = new User();
+		user.setUsername("new guy");
+		user.setEmail("newguy@example.com");
+
+		Action action = new Action();
+		action.setCode("34ae8c3f0142aa3bc4");
+
+		String subjectExpected = "Verify your e-mail address";
+		String textExpected = "Hello " + user.getUsername() + ",\n" + "your e-mail address has been changed in your profile settings.\n"
+				+ "Please confirm these changes by clicking the following link:\n" + "http://www.syncnapsis.com/test/activate/" + action.getCode()
+				+ "\n" + "Beste regards";
+		textExpected = textExpected.replace("\n", "\r\n"); // mailing replaced the line endings.
+
+		String toExpected = user.getEmail();
+
+		Wiser w = new Wiser();
+		w.start();
+
+		try
+		{
+			assertTrue(m.sendVerifyMailAddress(user, action));
+
+			assertEquals(1, w.getMessages().size());
+			MimeMessage msg = w.getMessages().get(0).getMimeMessage();
+
+			assertEquals(subjectExpected, msg.getSubject());
+			assertEquals(textExpected, msg.getContent());
+			assertEquals(1, msg.getAllRecipients().length);
+			assertEquals(1, msg.getRecipients(RecipientType.TO).length);
+			assertEquals(toExpected, ((InternetAddress) msg.getRecipients(RecipientType.TO)[0]).getAddress());
+			assertEquals(1, msg.getFrom().length);
+			assertEquals(m.getGlobalProperty(Mailer.KEY_FROM), ((InternetAddress) msg.getFrom()[0]).getAddress());
+		}
+		finally
+		{
+			w.stop();
+		}
+	}
+
+	public void testSendUserRoleChangedNotification() throws Exception
+	{
+		BaseApplicationMailer m = new BaseApplicationMailer(new File("target/test-classes/mail.properties"));
+
+		User user = new User();
+		user.setUsername("new guy");
+		user.setEmail("newguy@example.com");
+		user.setRoleExpireDate(new Date(1234));
+
+		UserRole newRole = new UserRole();
+		newRole.setRolename("newRole");
+		user.setRole(newRole);
+
+		UserRole oldRole = new UserRole();
+		oldRole.setRolename("oldRole");
+
+		String reason = "role expired";
+
+		String subjectExpected = "User-Role updated";
+		String textExpected = "Hello " + user.getUsername() + ",\n" + "your user-role has changed:\n" + "Reason: " + reason + "\n" + "Old role: "
+				+ oldRole.getRolename() + "\n" + "New role: " + user.getRole().getRolename() + "\n" + "Expires: " + user.getRoleExpireDate() + "\n"
+				+ "Best regards";
+		textExpected = textExpected.replace("\n", "\r\n"); // mailing replaced the line endings.
+
+		String toExpected = user.getEmail();
+
+		Wiser w = new Wiser();
+		w.start();
+
+		try
+		{
+			assertTrue(m.sendUserRoleChangedNotification(user, oldRole, reason));
+
+			assertEquals(1, w.getMessages().size());
+			MimeMessage msg = w.getMessages().get(0).getMimeMessage();
+
+			assertEquals(subjectExpected, msg.getSubject());
+			assertEquals(textExpected, msg.getContent());
+			assertEquals(1, msg.getAllRecipients().length);
+			assertEquals(1, msg.getRecipients(RecipientType.TO).length);
+			assertEquals(toExpected, ((InternetAddress) msg.getRecipients(RecipientType.TO)[0]).getAddress());
+			assertEquals(1, msg.getFrom().length);
+			assertEquals(m.getGlobalProperty(Mailer.KEY_FROM), ((InternetAddress) msg.getFrom()[0]).getAddress());
+		}
+		finally
+		{
+			w.stop();
+		}
+	}
+
+	public void testSendGeneralNotification() throws Exception
+	{
+		BaseApplicationMailer m = new BaseApplicationMailer(new File("target/test-classes/mail.properties"));
+
+		String notificationSubject = "a subject";
+		String notificationText = "a text for you: {user.username}";
+
+		User user1 = new User();
+		user1.setUsername("user1");
+		user1.setEmail("user1@example.com");
+
+		User user2 = new User();
+		user2.setUsername("user2");
+		user2.setEmail("user2@example.com");
+
+		List<User> users = new LinkedList<User>();
+		users.add(user1);
+		users.add(user2);
+
+		Wiser w = new Wiser();
+		w.start();
+
+		try
+		{
+			assertEquals(users.size(), m.sendGeneralNotification(users, notificationSubject, notificationText));
+
+			assertEquals(users.size(), w.getMessages().size());
+			MimeMessage msg;
+
+			for(int i = 0; i < w.getMessages().size(); i++)
+			{
+				msg = w.getMessages().get(i).getMimeMessage();
+
+				assertEquals(notificationSubject, msg.getSubject());
+				assertEquals(notificationText.replace("{user.username}", users.get(i).getUsername()), msg.getContent());
+				assertEquals(1, msg.getAllRecipients().length);
+				assertEquals(1, msg.getRecipients(RecipientType.TO).length);
+				assertEquals(users.get(i).getEmail(), ((InternetAddress) msg.getRecipients(RecipientType.TO)[0]).getAddress());
+				assertEquals(1, msg.getFrom().length);
+				assertEquals(m.getGlobalProperty(Mailer.KEY_FROM), ((InternetAddress) msg.getFrom()[0]).getAddress());
+			}
+		}
+		finally
+		{
+			w.stop();
+		}
+	}
+}
