84b52e664cd92d7c140831f1519114ec990572a6
ultimate <ultimatej@gmx.net>
2013-04-25 11:19:41 +0200

    Added: version batch file

---
+++ bin/s_version.bat
@@ -0,0 +1,15 @@
+@REM
+@REM Syncnapsis Framework - Copyright (c) 2012 ultimate
+@REM 
+@REM This program is free software; you can redistribute it and/or modify it under the terms of
+@REM the GNU General Public License as published by the Free Software Foundation; either version
+@REM 3 of the License, or any later version.
+@REM 
+@REM This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+@REM without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+@REM See the GNU General Public License for more details.
+@REM 
+@REM You should have received a copy of the GNU General Plublic License along with this program;
+@REM if not, see <http://www.gnu.org/licenses/>.
+@REM
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=%1
