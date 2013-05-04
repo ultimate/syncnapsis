bb599bf7192f0390bd536ce16091fa1c12fa5e87
ultimate <ultimatej@gmx.net>
2013-05-04 20:19:59 +0200

    Refactored random generation classes

--- src/main/java/com/syncnapsis/utils/data/DataGenerator.java
+++ src/main/java/com/syncnapsis/utils/data/DataGenerator.java
@@ -41,6 +41,11 @@ public abstract class DataGenerator implements InitializingBean
 {
 	protected transient final Logger	logger	= LoggerFactory.getLogger(getClass());
 
+	/**
+	 * The extended random number generator used to generate random values
+	 */
+	protected final ExtendedRandom		random	= new ExtendedRandom("syncnapsis".hashCode());
+
 	protected String					projectDirectory;
 	protected String[]					excludeTableList;
 
