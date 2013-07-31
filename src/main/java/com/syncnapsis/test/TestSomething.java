ec6631c57509ee1c6ca54486468ae7a0f0a00543
ultimate <ultimatej@gmx.net>
2013-07-31 18:10:03 +0200

    Extended: TestSomething for Galaxy-Stats

--- src/main/java/com/syncnapsis/test/TestSomething.java
+++ src/main/java/com/syncnapsis/test/TestSomething.java
@@ -19,17 +19,26 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+import com.syncnapsis.constants.UniverseConquestConstants;
+import com.syncnapsis.data.model.Galaxy;
+import com.syncnapsis.data.model.Parameter;
+import com.syncnapsis.data.model.SolarSystem;
+import com.syncnapsis.data.model.SolarSystemInfrastructure;
+import com.syncnapsis.data.model.SolarSystemPopulation;
 import com.syncnapsis.data.model.help.Vector;
+import com.syncnapsis.data.service.ParameterManager;
 import com.syncnapsis.tests.annotations.Untested;
 import com.syncnapsis.universe.Calculator;
 import com.syncnapsis.universe.CalculatorImpl;
 import com.syncnapsis.utils.FileUtil;
 import com.syncnapsis.utils.IconUtil;
+import com.syncnapsis.utils.MathUtil;
 
 @Untested
 @SuppressWarnings("unused")
@@ -37,26 +46,36 @@ public class TestSomething
 {
 	public static void main(String[] args) throws Exception
 	{
-		final Calculator calculator = new CalculatorImpl(null); // don't need ParameterManager
+		MockParameterManager mockParameterManager = new MockParameterManager();
+		mockParameterManager.values.put(UniverseConquestConstants.PARAM_SOLARSYSTEM_HABITABILITY_MAX, 1000);
+		mockParameterManager.values.put(UniverseConquestConstants.PARAM_SOLARSYSTEM_SIZE_MAX, 1000);
+		mockParameterManager.values.put(UniverseConquestConstants.PARAM_SOLARSYSTEM_MAX_POPULATION_FACTOR, 1000000);
+		mockParameterManager.values.put(UniverseConquestConstants.PARAM_TRAVEL_MAX_FACTOR, 10.0);
+		mockParameterManager.values.put(UniverseConquestConstants.PARAM_TRAVEL_EXODUS_FACTOR, 20.0);
+		final Calculator calculator = new CalculatorImpl(mockParameterManager);
 
 		File folder = new File("D:/info/syncnapsis/syncnapsis-examples/syncnapsis-examples-ui-playground/src/main/webapp");
 
 		Map<String, List<Vector.Integer>> galaxies = new HashMap<String, List<Vector.Integer>>();
 		List<Vector.Integer> sectors;
 		Vector.Integer size;
-		
+
 		StringBuilder output = new StringBuilder();
 
+		long max = 1000 * 1000 * 1000000L;
+
 		long start;
+		long maxMovable;
 		CalculationThread<Integer> maxGapT;
 		CalculationThread<Integer> avgGapT;
+		CalculationThread<Integer> minGapT;
 		for(File f : folder.listFiles())
 		{
 			if(f.getName().startsWith("sectors"))
 			{
 				System.out.print(f.getName());
 				output.append(f.getName());
-				
+
 				sectors = new ArrayList<Vector.Integer>(100000);
 
 				start = System.currentTimeMillis();
@@ -88,7 +107,7 @@ public class TestSomething
 				output.append("\t" + size.getY());
 				output.append("\t" + size.getZ());
 
-//				if(sectors.size() < 30000)
+				if(sectors.size() < 30000)
 				{
 					final List<Vector.Integer> s = sectors;
 					maxGapT = new CalculationThread<Integer>() {
@@ -103,17 +122,42 @@ public class TestSomething
 							return calculator.calculateAvgGap(s);
 						}
 					};
+					minGapT = new CalculationThread<Integer>() {
+						protected Integer calc()
+						{
+							return calculator.calculateMinGap(s);
+						}
+					};
 					maxGapT.start();
 					avgGapT.start();
-					output.append("\t" + maxGapT.getResult());
+					minGapT.start();
 					output.append("\t" + avgGapT.getResult());
+					output.append("\t" + maxGapT.getResult());
+					output.append("\t(" + MathUtil.round(maxGapT.getResult().doubleValue() / avgGapT.getResult().doubleValue(), -1) + ":1)");
+					output.append("\t" + minGapT.getResult());
+					output.append("\t(1:" + MathUtil.round(avgGapT.getResult().doubleValue() / minGapT.getResult().doubleValue(), -1) + ")");
+
+					SolarSystemPopulation originMax = getPopulation(max, 1, maxGapT.getResult());
+					SolarSystemPopulation originMax2 = getPopulation(max, max / 2, maxGapT.getResult());
+					SolarSystemPopulation originEq = getPopulation(max, max, maxGapT.getResult());
+					SolarSystemPopulation originMin2 = getPopulation(max / 2, max, maxGapT.getResult());
+					SolarSystemPopulation originMin = getPopulation(1, max, maxGapT.getResult());
+
+					maxMovable = calculator.calculateMaxMovablePopulation(originMin2, avgGapT.getResult());
+					output.append("\t" + MathUtil.round(maxMovable / (double) (max / 2), -2));
+
+					maxMovable = calculator.calculateMaxMovablePopulation(originMax, maxGapT.getResult());
+					output.append("\t" + MathUtil.round(maxMovable / (double) (max), -2));
+
+					maxMovable = calculator.calculateMaxMovablePopulation(originMin, minGapT.getResult());
+					output.append("\t" + MathUtil.round(maxMovable / (double) (1), -2));
 				}
 
 				output.append("\t(" + (System.currentTimeMillis() - start) + "ms)\n");
-				System.out.println("\t(" + (System.currentTimeMillis() - start) + "ms)\n");
+				System.out.print("\t(" + (System.currentTimeMillis() - start) + "ms)\n");
 			}
 		}
-		
+
 		FileUtil.writeFile(new File(folder.getAbsolutePath() + "/sector-stats.txt"), output.toString());
 
 		// createIconFile("ultimate");
@@ -125,6 +169,18 @@ public class TestSomething
 		// createIconFile("thing");
 		// createIconFile("abcdef");
 		// createIconFile("nothing");
+	}
+
+	private static SolarSystemPopulation getPopulation(long inf, long pop, int maxGap)
+	{
+		SolarSystemPopulation population = new SolarSystemPopulation();
+		population.setInfrastructure(new SolarSystemInfrastructure());
+		population.getInfrastructure().setInfrastructure(inf);
+		population.getInfrastructure().setSolarSystem(new SolarSystem());
+		population.getInfrastructure().getSolarSystem().setGalaxy(new Galaxy());
+		population.getInfrastructure().getSolarSystem().getGalaxy().setMaxGap(maxGap);
+		population.setPopulation(pop);
+		return population;
 	}
 
 	private static void createIconFile(String s) throws IOException
@@ -149,5 +205,249 @@ public class TestSomething
 			this.join();
 			return this.result;
 		}
+	}
+
+	private static class MockParameterManager implements ParameterManager
+	{
+		private Map<String, Object>	values	= new HashMap<String, Object>();
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericNameManager#getOrderedByName()
+		 */
+		@Override
+		public List<Parameter> getOrderedByName()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericNameManager#getOrderedByName(boolean)
+		 */
+		@Override
+		public List<Parameter> getOrderedByName(boolean returnOnlyActivated)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericNameManager#getByPrefix(java.lang.String, int,
+		 * boolean)
+		 */
+		@Override
+		public List<Parameter> getByPrefix(String prefix, int nRows, boolean returnOnlyActivated)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericNameManager#getByName(java.lang.String)
+		 */
+		@Override
+		public Parameter getByName(String name)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericNameManager#isNameAvailable(java.lang.String)
+		 */
+		@Override
+		public boolean isNameAvailable(String name)
+		{
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericNameManager#isNameValid(java.lang.String)
+		 */
+		@Override
+		public boolean isNameValid(String name)
+		{
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericManager#getAll()
+		 */
+		@Override
+		public List<Parameter> getAll()
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericManager#getAll(boolean)
+		 */
+		@Override
+		public List<Parameter> getAll(boolean returnOnlyActivated)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericManager#get(java.io.Serializable)
+		 */
+		@Override
+		public Parameter get(Long id)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericManager#getByIdList(java.util.List)
+		 */
+		@Override
+		public List<Parameter> getByIdList(List<Long> idList)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.GenericManager#exists(java.io.Serializable)
+		 */
+		@Override
+		public boolean exists(Long id)
+		{
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see
+		 * com.syncnapsis.data.service.GenericManager#save(com.syncnapsis.data.model.base.Identifiable
+		 * )
+		 */
+		@Override
+		public Parameter save(Parameter object)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see
+		 * com.syncnapsis.data.service.GenericManager#remove(com.syncnapsis.data.model.base.Identifiable
+		 * )
+		 */
+		@Override
+		public String remove(Parameter object)
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getString(java.lang.String)
+		 */
+		@Override
+		public String getString(String name)
+		{
+			return (String) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getLong(java.lang.String)
+		 */
+		@Override
+		public Long getLong(String name)
+		{
+			return (Long) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getInteger(java.lang.String)
+		 */
+		@Override
+		public Integer getInteger(String name)
+		{
+			return (Integer) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getShort(java.lang.String)
+		 */
+		@Override
+		public Short getShort(String name)
+		{
+			return (Short) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getByte(java.lang.String)
+		 */
+		@Override
+		public Byte getByte(String name)
+		{
+			return (Byte) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getDouble(java.lang.String)
+		 */
+		@Override
+		public Double getDouble(String name)
+		{
+			return (Double) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getFloat(java.lang.String)
+		 */
+		@Override
+		public Float getFloat(String name)
+		{
+			return (Float) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getBoolean(java.lang.String)
+		 */
+		@Override
+		public Boolean getBoolean(String name)
+		{
+			return (Boolean) values.get(name);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.data.service.ParameterManager#getDate(java.lang.String)
+		 */
+		@Override
+		public Date getDate(String name)
+		{
+			return (Date) values.get(name);
+		}
+
 	}
 }
