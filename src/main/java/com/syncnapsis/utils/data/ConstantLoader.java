8f04ff4df1b631eb21f511a7f6168e2a7faf6bbb
ultimate <ultimatej@gmx.net>
2013-10-28 18:03:18 +0100

    Added: Constant & ConstantLoader

---
+++ src/main/java/com/syncnapsis/utils/data/ConstantLoader.java
@@ -0,0 +1,209 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.syncnapsis.utils.data;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.syncnapsis.utils.ReflectionsUtil;
+import com.syncnapsis.utils.data.constants.Constant;
+import com.syncnapsis.utils.reflections.Field;
+import com.syncnapsis.utils.reflections.FieldCriterion;
+import com.syncnapsis.utils.spring.Bean;
+
+/**
+ * Class loading constants defined in specified classes.
+ * 
+ * @author ultimate
+ * 
+ * @param <R> - the raw type for the constants
+ */
+public abstract class ConstantLoader<R> extends Bean implements InitializingBean
+{
+	/**
+	 * Logger-instance
+	 */
+	protected static transient final Logger	logger	= LoggerFactory.getLogger(ConstantLoader.class);
+
+	/**
+	 * The raw type for the constants held by this loader
+	 */
+	protected Class<R>						constantRawType;
+
+	/**
+	 * The list of classes to scan for constants
+	 */
+	protected List<Class<?>>				constantClasses;
+
+	/**
+	 * The list of constants contained in the specified constant classes
+	 */
+	protected List<Constant<R>>				constants;
+
+	/**
+	 * Construct a new ConstantLoader with the given raw type.<br>
+	 * Note: remember to load constants using load(..) after setting the constant classes.
+	 * 
+	 * @param constantRawType - the constant raw type
+	 */
+	public ConstantLoader(Class<R> constantRawType)
+	{
+		this.constantRawType = constantRawType;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	@Override
+	public void afterPropertiesSet()
+	{
+		load();
+	}
+
+	/**
+	 * The list of classes to scan for constants
+	 * 
+	 * @param constantClasses - the list of classes
+	 */
+	public void setConstantClasses(List<Class<?>> constantClasses)
+	{
+		this.constantClasses = constantClasses;
+	}
+
+	/**
+	 * The list of classes to scan for constants
+	 * 
+	 * @param constantClassesStrings - the list of classes as a String array
+	 * @throws ClassNotFoundException - if a class could not be loaded from the given strings
+	 */
+	public void setConstantClasses(String... constantClassesStrings) throws ClassNotFoundException
+	{
+		this.constantClasses = new ArrayList<Class<?>>(constantClassesStrings.length);
+		for(String cls : constantClassesStrings)
+		{
+			this.constantClasses.add(Class.forName(cls));
+		}
+	}
+
+	/**
+	 * The list of classes to scan for constants
+	 * 
+	 * @return constantClasses
+	 */
+	public List<Class<?>> getConstantClasses()
+	{
+		return constantClasses;
+	}
+
+	/**
+	 * The raw type for the constants held by this loader
+	 * 
+	 * @return constantRawType
+	 */
+	public Class<R> getConstantRawType()
+	{
+		return constantRawType;
+	}
+
+	/**
+	 * The list of constants contained in the specified constant classes
+	 * 
+	 * @return contants
+	 */
+	public List<Constant<R>> getConstants()
+	{
+		return constants;
+	}
+
+	/**
+	 * Load all constants contained in the specified constant classes.
+	 */
+	public void load()
+	{
+		if(this.constants == null)
+			this.constants = scanClasses(this.constantClasses, this.constantRawType);
+		for(Constant<R> c : constants)
+			load(c);
+	}
+
+	/**
+	 * Load a specific constant and define its value.
+	 * 
+	 * @param constant - the constant to load/define
+	 */
+	public abstract void load(Constant<R> constant);
+
+	/**
+	 * Scan the list of given classes for constants of the given type.
+	 * 
+	 * @param constantClasses - the list of classes to scan for constants
+	 * @param rawType - the raw type of constants to look for
+	 * @return the list of constants found
+	 */
+	@SuppressWarnings("unchecked")
+	public static <R> List<Constant<R>> scanClasses(List<Class<?>> constantClasses, Class<R> rawType)
+	{
+		List<Constant<R>> constants = new LinkedList<Constant<R>>();
+
+		List<Field> fields;
+		Constant<R> constant;
+		for(Class<?> cls : constantClasses)
+		{
+			fields = ReflectionsUtil.findFields(cls, FieldCriterion.STATIC, FieldCriterion.FINAL);
+
+			for(Field f : fields)
+			{
+				if(!Constant.class.isAssignableFrom(f.getField().getType()))
+					continue;
+				Class<?> genericType;
+				if(f.getField().getGenericType() instanceof Class)
+					genericType = (Class<?>) ReflectionsUtil.getActualTypeArguments(f.getField().getType(), Constant.class)[0];
+				else if(f.getField().getGenericType() instanceof ParameterizedType)
+					genericType = (Class<?>) ((ParameterizedType) f.getField().getGenericType()).getActualTypeArguments()[0];
+				else
+					continue;
+				if(!rawType.isAssignableFrom(genericType))
+					continue;
+				try
+				{
+
+					constant = (Constant<R>) ReflectionsUtil.getField(null, f.getField());
+					if(!constants.contains(constant))
+					{
+						constants.add(constant);
+						logger.debug("constant found: " + cls.getSimpleName() + "#" + f.getName() + " : " + constant.getName());
+					}
+				}
+				catch(IllegalArgumentException e)
+				{
+					logger.error("IllegalArgumentException?!", e);
+				}
+				catch(IllegalAccessException e)
+				{
+					logger.error("could not access constant:" + cls.getName() + "#" + f.getName());
+				}
+			}
+		}
+
+		return new ArrayList<Constant<R>>(constants);
+	}
+}
---
+++ src/main/java/com/syncnapsis/utils/data/constants/Constant.java
@@ -0,0 +1,231 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.syncnapsis.utils.data.constants;
+
+import com.syncnapsis.utils.data.ConstantLoader;
+
+
+/**
+ * Class representing a simple constant usable in applications.<br>
+ * Constants will only be editable using {@link ConstantLoader}s which provide a function to load
+ * constants from a data source.<br>
+ * <br>
+ * <b>Note: the type-specific getters may throw a null-pointer if the raw value for this constant
+ * cannot be converted into that type.</b>
+ * 
+ * @author ultimate
+ * 
+ * @param <R> - the raw type for this constant
+ */
+public abstract class Constant<R>
+{
+	/**
+	 * The name of this constant
+	 */
+	protected String	name;
+
+	/**
+	 * The raw value
+	 */
+	protected R			value_Raw;
+
+	/**
+	 * The value as a String
+	 */
+	protected String	value_String;
+	/**
+	 * The value as an Integer
+	 */
+	protected Integer	value_Integer;
+	/**
+	 * The value as an Long
+	 */
+	protected Long		value_Long;
+	/**
+	 * The value as an Double
+	 */
+	protected Double	value_Double;
+	/**
+	 * The value as an Float
+	 */
+	protected Float		value_Float;
+	/**
+	 * The value as an Short
+	 */
+	protected Short		value_Short;
+	/**
+	 * The value as an Byte
+	 */
+	protected Byte		value_Byte;
+	/**
+	 * The value as an Boolean
+	 */
+	protected Boolean	value_Boolean;
+	/**
+	 * The value as an Integer
+	 */
+	protected Character	value_Character;
+
+	/**
+	 * Initiate a new constant
+	 * 
+	 * @param name - the name of this constant.
+	 */
+	public Constant(String name)
+	{
+		this(name, null);
+	}
+
+	/**
+	 * Initiate a new constant
+	 * 
+	 * @param name - the name of this constant.
+	 * @param defaultRawValue - the default raw value
+	 */
+	public Constant(String name, R defaultRawValue)
+	{
+		this.name = name;
+		define(defaultRawValue);
+	}
+
+	/**
+	 * The name of this constant
+	 * 
+	 * @return name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+	
+	/**
+	 * Set the raw value (and all type specific values) for this constant to null
+	 */
+	/*package*/ void defineNull()
+	{
+		this.value_Raw = null;
+		this.value_String = null;
+		this.value_Long = null;
+		this.value_Integer = null;
+		this.value_Double = null;
+		this.value_Float = null;
+		this.value_Short = null;
+		this.value_Byte = null;
+		this.value_Character = null;
+		this.value_Boolean = null;
+	}
+
+	/**
+	 * Set the raw value for this constant an initiate all convertible values of other types.
+	 * 
+	 * @param value
+	 */
+	/* package */abstract void define(R raw);
+
+	public R raw()
+	{
+		return value_Raw;
+	}
+
+	/**
+	 * Get the value as a String
+	 * 
+	 * @return value
+	 */
+	public String asString()
+	{
+		return value_String;
+	}
+
+	/**
+	 * Get the value as an int
+	 * 
+	 * @return value
+	 */
+	public int asInt()
+	{
+		return value_Integer;
+	}
+
+	/**
+	 * Get the value as a long
+	 * 
+	 * @return value
+	 */
+	public long asLong()
+	{
+		return value_Long;
+	}
+
+	/**
+	 * Get the value as a double
+	 * 
+	 * @return value
+	 */
+	public double asDouble()
+	{
+		return value_Double;
+	}
+
+	/**
+	 * Get the value as a float
+	 * 
+	 * @return value
+	 */
+	public float asFloat()
+	{
+		return value_Float;
+	}
+
+	/**
+	 * Get the value as a short
+	 * 
+	 * @return value
+	 */
+	public short asShort()
+	{
+		return value_Short;
+	}
+
+	/**
+	 * Get the value as a byte
+	 * 
+	 * @return value
+	 */
+	public byte asByte()
+	{
+		return value_Byte;
+	}
+
+	/**
+	 * Get the value as a boolean
+	 * 
+	 * @return value
+	 */
+	public boolean asBoolean()
+	{
+		return value_Boolean;
+	}
+
+	/**
+	 * Get the value as a char
+	 * 
+	 * @return value
+	 */
+	public char asChar()
+	{
+		return value_Character;
+	}
+}
