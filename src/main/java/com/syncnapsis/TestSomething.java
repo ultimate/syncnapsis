74502e50f067fb8f505bd109509a80104729c072
ultimate <ultimatej@gmx.net>
2013-12-19 16:29:29 +0100

    Added: java3d.vecmath dependency

--- src/main/java/com/syncnapsis/TestSomething.java
+++ src/main/java/com/syncnapsis/TestSomething.java
@@ -1,56 +1,31 @@
 package com.syncnapsis;
 
-import java.util.Random;
+import javax.vecmath.Matrix4d;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
 
 public class TestSomething
 {
 	public static void main(String[] args)
-	{
-		Random r = new Random();
-		long seed1 = System.currentTimeMillis();
-		Random rS11 = new Random(seed1);
-		Random rS12 = new Random(seed1); 
-		long seed2 = "syncnapsis".hashCode();
-		Random rS21 = new Random (seed2);
-		Random rS22 = new Random (seed2);
-		Random rS23 = new Random (seed2);
-		Random rS24 = new Random (seed2);
+	{		
+		Point3d p = new Point3d(1, 0.5, 0);
+		Matrix4d m = new Matrix4d();
+		System.out.println(p);		
+		System.out.println(m);
 		
-		print1(r);
-		print1(rS11);
-		print1(rS12);
-		print1(rS21);
-		print1(rS22);
-		print2(rS23);
-		print3(rS24);
-	}
-	
-	private static void print1(Random r)
-	{
-		System.out.print(r.nextInt() + "   ");
-		System.out.print(r.nextDouble() + "   ");
-		System.out.print(r.nextFloat() + "   ");
-		System.out.print(r.nextGaussian() + "   ");
-		System.out.print(r.nextInt() + "   ");
-		System.out.println("-----------");
-	}
-	private static void print2(Random r)
-	{
-		System.out.print(r.nextInt() + "   ");
-		System.out.print(r.nextLong() + "   ");
-		System.out.print(r.nextFloat() + "   ");
-		System.out.print(r.nextLong() + r.nextLong() + "   ");
-		System.out.print(r.nextInt() + "   ");
-		System.out.println("-----------");
-	}
-	private static void print3(Random r)
-	{
-		System.out.print(r.nextLong() + "   ");
-		System.out.print(r.nextInt() + "   ");
-		System.out.print(r.nextInt() + "   ");
-		System.out.print(r.nextLong() + r.nextLong() + "   ");
-		System.out.print(r.nextInt() + "   ");
-		System.out.println("-----------");
-	}
-	
+		System.out.println("rotZ");
+		m.rotZ(Math.PI/2);		
+		m.transform(p);
+		System.out.println(p);	
+
+		System.out.println("rotX");
+		m.rotX(Math.PI/2);		
+		m.transform(p);
+		System.out.println(p);		
+
+		System.out.println("shift");
+		m.setTranslation(new Vector3d(2, 3, 4));
+		m.transform(p);
+		System.out.println(p);		
+	}	
 }
