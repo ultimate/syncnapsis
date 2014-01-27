4029f37e691110c14f43b9606cefeb4bd1e0b92c
ultimate <ultimatej@gmx.net>
2014-01-27 14:46:29 +0100

    Updated: Distributions to make inverted mode work

--- src/main/java/com/syncnapsis/utils/math/Distribution.java
+++ src/main/java/com/syncnapsis/utils/math/Distribution.java
@@ -12,7 +12,16 @@ public abstract class Distribution
 	/**
 	 * The default random number generator
 	 */
-	protected static final Random	random	= new Random();
+	protected static final Random	random		= new Random();
+
+	/**
+	 * The precision for inverse calculations
+	 */
+	private static final int		PRECISION	= 1000000;
+	/**
+	 * The sum array used for inverse calculations
+	 */
+	private double[]				sum			= null;
 
 	/**
 	 * Return the relative probability of a number x.<br>
@@ -41,6 +50,44 @@ public abstract class Distribution
 	public double next()
 	{
 		return next(random);
+	}
+
+	/**
+	 * Inverse find the corresponding value for the given probability sum being obtained from a
+	 * uniformly distributed random function. This way we can reproduce the distribution defined
+	 * by {@link Distribution#probability(double)} from a uniform distribution.
+	 * 
+	 * @param sum - the sum to invert to a random value according to this distribution
+	 * @return the random value according to this distribution
+	 */
+	protected double invert(double sum)
+	{
+		double eps = 1 / (double) PRECISION;
+
+		if(this.sum == null)
+		{
+			this.sum = new double[PRECISION + 1];
+			for(int i = 1; i <= PRECISION; i++)
+				this.sum[i] = this.sum[i - 1] + probability(i * eps - eps / 2) * eps;
+		}
+
+		int i = PRECISION / 2;
+		int step = i;
+		do
+		{
+			step = (step + 1) / 2;
+
+			if(this.sum[i] < sum)
+				i += step;
+			else if(this.sum[i - 1] > sum)
+				i -= step;
+			else
+				break;
+			if(step == 1)
+				break;
+		} while(true);
+
+		return i * eps - eps / 2;
 	}
 
 	/**
@@ -112,11 +159,15 @@ public abstract class Distribution
 		/**
 		 * The exponent
 		 */
-		private double			exponent;
+		private int				exponent;
 		/**
 		 * Increasing or decreasing mode?
 		 */
 		private PolynomialMode	mode;
+		/**
+		 * Invert the probability curve?
+		 */
+		private boolean			inverted;
 
 		/**
 		 * Constructor
@@ -124,11 +175,12 @@ public abstract class Distribution
 		 * @param exponent - The exponent
 		 * @param mode - Increasing or decreasing mode?
 		 */
-		public Polynomial(double exponent, PolynomialMode mode)
+		public Polynomial(int exponent, PolynomialMode mode, boolean inverted)
 		{
 			super();
 			this.exponent = exponent;
 			this.mode = mode;
+			this.inverted = inverted;
 		}
 
 		/*
@@ -139,64 +191,48 @@ public abstract class Distribution
 		public double probability(double x)
 		{
 			x = Math.abs(x);
-			if(mode == PolynomialMode.INCREASING)
+			double ret = 0;
+			if(x >= 0 && x < 1)
 			{
-				if(x >= 0 && x < 1)
-					return Math.pow(x, exponent);
-				else
-					return 0;
+				if(mode == PolynomialMode.INCREASING)
+					ret = Math.pow(x, exponent);
+				else if(mode == PolynomialMode.DECREASING)
+					ret = Math.pow(1 - x, exponent);
+
+				if(inverted)
+					ret = 1 - ret;
 			}
-			else if(mode == PolynomialMode.DECREASING)
-			{
-				if(x >= 0 && x < 1)
-					return Math.pow(1 - x, exponent);
-				else
-					return 0;
-			}
-			else if(mode == PolynomialMode.INCREASING_INVERTED)
-			{
-				if(x >= 0 && x < 1)
-					return 1 - Math.pow(1 - x, exponent);
-				else
-					return 0;
-			}
-			else if(mode == PolynomialMode.DECREASING_INVERTED)
-			{
-				if(x >= 0 && x < 1)
-					return 1 - Math.pow(x, exponent);
-				else
-					return 0;
-			}
-			else
-			{
-				return 0;
-			}
+			return ret;
 		}
 
+		/*
+		 * (non-Javadoc)
+		 * @see com.syncnapsis.utils.math.Distribution#next(java.util.Random)
+		 */
 		@Override
 		public double next(Random random)
 		{
 			double d = UNIFORM.next(random);
-			double ret;
-			if(mode == PolynomialMode.INCREASING)
+			double sign = Math.signum(d);
+			d = Math.abs(d);
+
+			double ret = 0;
+
+			if(!inverted || exponent == 1)
 			{
-				ret = Math.signum(d) * Math.pow(Math.abs(d), 1 / (exponent + 1));
-			}
-			else if(mode == PolynomialMode.DECREASING)
-			{
-				ret = Math.signum(d) * (1 - Math.pow(Math.abs(d), 1 / (exponent + 1)));
-			}
-			else if(mode == PolynomialMode.INCREASING_INVERTED)
-			{
-				ret = Math.signum(d) * (Math.abs(d) + Math.pow(Math.abs(d), 1 / (exponent + 1))) / 2;
-			}
-			else if(mode == PolynomialMode.DECREASING_INVERTED)
-			{
-				ret = Math.signum(d) * (1 - (Math.abs(d) + Math.pow(Math.abs(d), 1 / (exponent + 1))) / 2);
+				if(mode == PolynomialMode.INCREASING)
+				{
+					ret = sign * Math.pow(d, 1 / (double) (exponent + 1));
+				}
+				else if(mode == PolynomialMode.DECREASING)
+				{
+					ret = sign * (1 - Math.pow(d, 1 / (double) (exponent + 1)));
+				}
 			}
 			else
 			{
-				ret = 0;
+				d *= exponent / (double) (exponent + 1);
+				ret = sign * invert(d);
 			}
 			return ret;
 		}
@@ -204,54 +240,50 @@ public abstract class Distribution
 
 	private enum PolynomialMode
 	{
-		INCREASING, DECREASING, INCREASING_INVERTED, DECREASING_INVERTED
+		INCREASING, DECREASING
 	}
 
 	/**
 	 * A linear distribution (increasing)
 	 */
-	public static final Distribution	LINEAR_INCREASING			= new Polynomial(1, PolynomialMode.INCREASING);
+	public static final Distribution	LINEAR_INCREASING			= new Polynomial(1, PolynomialMode.INCREASING, false);
 	/**
 	 * A linear distribution (decreasing)
 	 */
-	public static final Distribution	LINEAR_DECREASING			= new Polynomial(1, PolynomialMode.DECREASING);
+	public static final Distribution	LINEAR_DECREASING			= new Polynomial(1, PolynomialMode.DECREASING, false);
 	/**
 	 * A square distribution (increasing)
 	 */
-	public static final Distribution	SQUARE_INCREASING			= new Polynomial(2, PolynomialMode.INCREASING);
+	public static final Distribution	SQUARE_INCREASING			= new Polynomial(2, PolynomialMode.INCREASING, false);
 	/**
 	 * A square distribution (decreasing)
 	 */
-	public static final Distribution	SQUARE_DECREASING			= new Polynomial(2, PolynomialMode.DECREASING);
+	public static final Distribution	SQUARE_DECREASING			= new Polynomial(2, PolynomialMode.DECREASING, false);
 	/**
 	 * A cubic distribution (increasing)
 	 */
-	public static final Distribution	CUBIC_INCREASING			= new Polynomial(3, PolynomialMode.INCREASING);
+	public static final Distribution	CUBIC_INCREASING			= new Polynomial(3, PolynomialMode.INCREASING, false);
 	/**
 	 * A cubic distribution (decreasing)
 	 */
-	public static final Distribution	CUBIC_DECREASING			= new Polynomial(3, PolynomialMode.DECREASING);
+	public static final Distribution	CUBIC_DECREASING			= new Polynomial(3, PolynomialMode.DECREASING, false);
 
 	// inverted modes
 
 	/**
-	 * A square distribution (increasing, inverted)<br>
-	 * <b>*** buggy ***</b>
+	 * A square distribution (increasing, inverted)
 	 */
-	public static final Distribution	SQUARE_INCREASING_INVERTED	= new Polynomial(2, PolynomialMode.INCREASING_INVERTED);
+	public static final Distribution	SQUARE_INCREASING_INVERTED	= new Polynomial(2, PolynomialMode.INCREASING, true);
 	/**
-	 * A square distribution (decreasing, inverted)<br>
-	 * <b>*** buggy ***</b>
+	 * A square distribution (decreasing, inverted)
 	 */
-	public static final Distribution	SQUARE_DECREASING_INVERTED	= new Polynomial(2, PolynomialMode.DECREASING_INVERTED);
+	public static final Distribution	SQUARE_DECREASING_INVERTED	= new Polynomial(2, PolynomialMode.DECREASING, true);
 	/**
-	 * A cubic distribution (increasing, inverted)<br>
-	 * <b>*** buggy ***</b>
+	 * A cubic distribution (increasing, inverted)
 	 */
-	public static final Distribution	CUBIC_INCREASING_INVERTED	= new Polynomial(3, PolynomialMode.INCREASING_INVERTED);
+	public static final Distribution	CUBIC_INCREASING_INVERTED	= new Polynomial(3, PolynomialMode.INCREASING, true);
 	/**
-	 * A cubic distribution (decreasing, inverted)<br>
-	 * <b>*** buggy ***</b>
+	 * A cubic distribution (decreasing, inverted)
 	 */
-	public static final Distribution	CUBIC_DECREASING_INVERTED	= new Polynomial(3, PolynomialMode.DECREASING_INVERTED);
+	public static final Distribution	CUBIC_DECREASING_INVERTED	= new Polynomial(3, PolynomialMode.DECREASING, true);
 }
--- src/test/java/com/syncnapsis/utils/math/DistributionTest.java
+++ src/test/java/com/syncnapsis/utils/math/DistributionTest.java
@@ -10,132 +10,170 @@ public class DistributionTest extends LoggerTestCase
 		int steps = 20;
 		int drawScale = 200;
 		int samples = 1000000;
-		double prop, sample;
+		double prob, sample;
 		int[] sampleCount = new int[steps * 4 + 1];
+		double[] probs = new double[sampleCount.length];
+		double[] ratios = new double[sampleCount.length];
 		int index;
 		int skipped = 0;
 
-		logger.debug("Probabilities:");
+		StringBuffer buffer;
+
+		logger.info("Probabilities:");
 		for(int i = -steps; i <= steps; i++)
 		{
-			prop = distribution.probability(i / (double) steps);
-			
-			System.out.print(i / (double) steps + "\t");
+			prob = distribution.probability(i / (double) steps);
+			probs[i + 2 * steps] = prob;
+
+			buffer = new StringBuffer();
+			buffer.append(i / (double) steps + "\t" + prob + "\t");
 			for(int j = 0; j < drawScale; j++)
 			{
-				if(j / (double) drawScale < prop)
-					System.out.print("x");
+				if(j / (double) drawScale < prob)
+					buffer.append('x');
 			}
-			System.out.println("");
+			logger.debug(buffer.toString());
 		}
 
-		logger.debug("Values:");
+		logger.info("Values:");
+		long start = System.currentTimeMillis();
 		for(int i = 0; i < samples; i++)
 		{
 			sample = distribution.next();
-			index = (int) Math.round(sample * steps) + 2*steps;
+			index = (int) Math.round(sample * steps) + 2 * steps;
 			if(index < 0 || index >= sampleCount.length)
 			{
 				skipped++;
-//				logger.debug("skipping value: " + sample);
 				continue;
 			}
 			sampleCount[index]++;
 		}
-		logger.debug("(skipped = " + skipped + ")");
+		long end = System.currentTimeMillis();
+		logger.info("time needed: " + (end - start) + " / skipped: " + skipped);
 
+		double x;
 		for(int i = 0; i < sampleCount.length; i++)
 		{
-			System.out.print((i - 2*steps) / (double) steps + "\t");
+			ratios[i] = sampleCount[i] / probs[i];
+			x = (i - 2 * steps) / (double) steps;
+
+			buffer = new StringBuffer();
+			buffer.append(x + "\t" + ratios[i] + "\t");
+
 			for(int j = 0; j < sampleCount[i]; j += 200)
 			{
-				System.out.print("x");
+				buffer.append('x');
 			}
-			System.out.println("");
+
+			logger.debug(buffer.toString());
+		}
+
+		double avgRatio = 0;
+		int ratiosCounted = 0;
+		for(int i = steps + 1; i < sampleCount.length - steps; i++)
+		{
+			if(Double.isInfinite(ratios[i]) || Double.isNaN(ratios[i]))
+				continue;
+
+			avgRatio += ratios[i];
+			ratiosCounted++;
+		}
+		avgRatio /= ratiosCounted;
+		double ratioDelta = avgRatio * 0.10; // 10 % delta is allowed!
+
+		for(int i = steps + 1; i < sampleCount.length - steps; i++)
+		{
+			x = (i - 2 * steps) / (double) steps;
+
+			if(Double.isInfinite(ratios[i]) || Double.isNaN(ratios[i]) || sampleCount[i] < 1000)
+				// sampleCount < 1000 has too large rounding error 
+				continue;
+			
+			assertEquals("ratio delta too large: x=" + x + " avg=" + avgRatio + " ratio=" + ratios[i], avgRatio, ratios[i], ratioDelta);
 		}
 	}
 
 	@TestCoversMethods({ "probability", "next" })
 	public void test_UNIFORM() throws Exception
 	{
-		logger.debug("UNIFORM");
+		logger.info("UNIFORM");
 		distributionTest(Distribution.UNIFORM);
 	}
 
 	@TestCoversMethods({ "probability", "next" })
 	public void test_GAUSSIAN() throws Exception
 	{
-		logger.debug("GAUSSIAN");
+		logger.info("GAUSSIAN");
 		distributionTest(Distribution.GAUSSIAN);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_LINEAR_INCREASING() throws Exception
 	{
-		logger.debug("LINEAR_INCREASING");
+		logger.info("LINEAR_INCREASING");
 		distributionTest(Distribution.LINEAR_INCREASING);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_LINEAR_DECREASING() throws Exception
 	{
-		logger.debug("LINEAR_DECREASING");
+		logger.info("LINEAR_DECREASING");
 		distributionTest(Distribution.LINEAR_DECREASING);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_SQUARE_INCREASING() throws Exception
 	{
-		logger.debug("SQUARE_INCREASING");
+		logger.info("SQUARE_INCREASING");
 		distributionTest(Distribution.SQUARE_INCREASING);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_SQUARE_DECREASING() throws Exception
 	{
-		logger.debug("SQUARE_DECREASING");
+		logger.info("SQUARE_DECREASING");
 		distributionTest(Distribution.SQUARE_DECREASING);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_CUBIC_INCREASING() throws Exception
 	{
-		logger.debug("CUBIC_INCREASING");
+		logger.info("CUBIC_INCREASING");
 		distributionTest(Distribution.CUBIC_INCREASING);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_CUBIC_DECREASING() throws Exception
 	{
-		logger.debug("CUBIC_DECREASING");
+		logger.info("CUBIC_DECREASING");
 		distributionTest(Distribution.CUBIC_DECREASING);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_SQUARE_INCREASING_INVERTED() throws Exception
 	{
-		logger.debug("SQUARE_INCREASING_INVERTED");
+		logger.info("SQUARE_INCREASING_INVERTED");
 		distributionTest(Distribution.SQUARE_INCREASING_INVERTED);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_SQUARE_DECREASING_INVERTED() throws Exception
 	{
-		logger.debug("SQUARE_DECREASING_INVERTED");
+		logger.info("SQUARE_DECREASING_INVERTED");
 		distributionTest(Distribution.SQUARE_DECREASING_INVERTED);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_CUBIC_INCREASING_INVERTED() throws Exception
 	{
-		logger.debug("CUBIC_INCREASING_INVERTED");
+		logger.info("CUBIC_INCREASING_INVERTED");
 		distributionTest(Distribution.CUBIC_INCREASING_INVERTED);
 	}
-	
+
 	@TestCoversMethods({ "probability", "next" })
 	public void test_CUBIC_DECREASING_INVERTED() throws Exception
 	{
-		logger.debug("CUBIC_DECREASING_INVERTED");
+		logger.info("CUBIC_DECREASING_INVERTED");
 		distributionTest(Distribution.CUBIC_DECREASING_INVERTED);
 	}
 }
