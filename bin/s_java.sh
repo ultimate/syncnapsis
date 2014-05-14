8b234ebf317ffff9d0ec2f7748e2775039d442aa
ultimate <ultimatej@gmx.net>
2014-05-14 12:33:21 +0200

    fixed shell

--- bin/s_java.sh
+++ bin/s_java.sh
@@ -22,59 +22,60 @@
 # ==== START VALIDATION ====
 error=0
 # check JAVA_HOME
-if ["$JAVA_HOME" = ""]
+if [ "$JAVA_HOME" = "" ]
 then
-	echo "ERROR: JAVA_HOME not found in your environment!"
-	error=1
+        echo ERROR: JAVA_HOME not found in your environment!
+        error=1
 fi
 # check java executable
-if [! error -a ! -f $JAVA_HOME/bin/java]
+java -version > /dev/null 2>&1
+if [ !$error ] && [ $? != 0 ]
 then
-	echo "ERROR: JAVA_HOME is invalid: /bin/java not found in $JAVA_HOME"
-	error=1
+        echo ERROR: JAVA_HOME is invalid: /bin/java not found in $JAVA_HOME
+        error=1
 fi
 # check S_HOME
-if ["$S_HOME" = ""]
+if [ !$error ] && [ "$S_HOME" = "" ]
 then
-	echo "ERROR: S_HOME not found in your environment!"
-	error=1
+        echo ERROR: S_HOME not found in your environment!
+        error=1
 fi
 # check s_java.sh
-if [! error -a ! -f $S_HOME/bin/s_java.sh]
+if [ !$error ] && [ ! -f $S_HOME/bin/s_java.sh ]
 then
-	echo "ERROR: JAVA_HOME is invalid: /bin/s_java.sh not found in $S_HOME"
-	error=1
+        echo ERROR: S_HOME is invalid: /bin/s_java.sh not found in $S_HOME
+        error=1
 fi
 # ==== END VALIDATION ====
 
-if [!error]
+if [ !$error ]
 then
 # do the stuff
 	S_ARGS=$*
 	S_JAVA_EXE=$JAVA_HOME/bin/java
-	S_CP=.;target/classes
+	S_CP=.:target/classes
 	# syncnapsis dependencies
-	S_CP=$S_CP;$S_HOME/syncnapsis-core/syncnapsis-core-application-base/target/classes
-	S_CP=$S_CP;$S_HOME/syncnapsis-core/syncnapsis-core-data/target/classes
-	S_CP=$S_CP;$S_HOME/syncnapsis-core/syncnapsis-core-game-base/target/classes
-	S_CP=$S_CP;$S_HOME/syncnapsis-core/syncnapsis-core-websockets/target/classes
-	S_CP=$S_CP;$S_HOME/syncnapsis-core/syncnapsis-core-utils/target/classes
-	S_CP=$S_CP;$S_HOME/syncnapsis-dev/syncnapsis-dev-utils/target/classes
+	S_CP=$S_CP:$S_HOME/syncnapsis-core/syncnapsis-core-application-base/target/classes
+	S_CP=$S_CP:$S_HOME/syncnapsis-core/syncnapsis-core-data/target/classes
+	S_CP=$S_CP:$S_HOME/syncnapsis-core/syncnapsis-core-game-base/target/classes
+	S_CP=$S_CP:$S_HOME/syncnapsis-core/syncnapsis-core-websockets/target/classes
+	S_CP=$S_CP:$S_HOME/syncnapsis-core/syncnapsis-core-utils/target/classes
+	S_CP=$S_CP:$S_HOME/syncnapsis-dev/syncnapsis-dev-utils/target/classes
 	# m2_repo dependencies
 	SLF4J_VERSION=1.6.4
-	S_CP=$S_CP;$M2_REPO/org/slf4j/slf4j-api/$SLF4J_VERSION/slf4j-api-$SLF4J_VERSION.jar
-	S_CP=$S_CP;$M2_REPO/org/slf4j/slf4j-log4j12/$SLF4J_VERSION/slf4j-log4j12-$SLF4J_VERSION.jar
+	S_CP=$S_CP:$M2_REPO/org/slf4j/slf4j-api/$SLF4J_VERSION/slf4j-api-$SLF4J_VERSION.jar
+	S_CP=$S_CP:$M2_REPO/org/slf4j/slf4j-log4j12/$SLF4J_VERSION/slf4j-log4j12-$SLF4J_VERSION.jar
 	LOG4J_VERSION=1.2.16
-	S_CP=$S_CP;$M2_REPO/log4j/log4j/$LOG4J_VERSION/log4j-%LOG4J_VERSION.jar
+	S_CP=$S_CP:$M2_REPO/log4j/log4j/$LOG4J_VERSION/log4j-%LOG4J_VERSION.jar
 	SPRING_VERSION=3.1.0.RELEASE
-	S_CP=$S_CP;$M2_REPO/org/springframework/spring-core/$SPRING_VERSION/spring-core-$SPRING_VERSION.jar
+	S_CP=$S_CP:$M2_REPO/org/springframework/spring-core/$SPRING_VERSION/spring-core-$SPRING_VERSION.jar
 	POSTGRESQL_VERSION=9.1-901.jdbc4
-	S_CP=$S_CP;$M2_REPO/postgresql/postgresql/$POSTGRESQL_VERSION/postgresql-$POSTGRESQL_VERSION.jar
+	S_CP=$S_CP:$M2_REPO/postgresql/postgresql/$POSTGRESQL_VERSION/postgresql-$POSTGRESQL_VERSION.jar
 	set -x
 	$S_JAVA_EXE -cp $S_CP $S_ARGS
 fi
 
-if [$? -eq 1]
+if [ $? -eq 1 ]
 then
 	error=1
 fi
