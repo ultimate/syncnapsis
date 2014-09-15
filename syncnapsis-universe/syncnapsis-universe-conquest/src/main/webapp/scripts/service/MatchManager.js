165e50fc3d8f2c92e303f95b675cdc2d9bccc511
ultimate <ultimatej@gmx.net>
2014-09-15 17:45:58 +0200

    Added MatchManager.js (e.g. for #111)

---
+++ syncnapsis-universe/syncnapsis-universe-conquest/src/main/webapp/scripts/service/MatchManager.js
@@ -0,0 +1,27 @@
+/**
+ * Syncnapsis Framework - Copyright (c) 2012-2014 ultimate
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version
+ * 3 of the License, or any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MECHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Plublic License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+//@requires("GenericManager")
+//@requires("Client")
+
+MatchManager = function()
+{
+	this.getAll = function()
+	{
+		// return match-list
+		return function(matches) { client.uiManager.onMatchesLoaded(matches); };
+	};
+};
+
+MatchManager.prototype = new GenericManager();
