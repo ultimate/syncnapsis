2e3a72b8ac0321aad88db7c4842152202205ab93
ultimate <ultimatej@gmx.net>
2014-11-24 12:45:51 +0100

    implementing comboselect #271

--- syncnapsis-universe/syncnapsis-universe-conquest/src/main/webapp/scripts/comboselect.js
+++ syncnapsis-universe/syncnapsis-universe-conquest/src/main/webapp/scripts/comboselect.js
@@ -24,10 +24,15 @@ ComboSelect = function(sourceElement, targetElement)
 	else
 		this.targetElement = targetElement;
 	
-	this.sourceElement.classList.add("comboselect");
-	this.sourceElement.classList.add("source");
-	this.targetElement.classList.add("comboselect");
-	this.targetElement.classList.add("target");
+	this.sourceList = document.createElement("ul");
+	this.sourceList.classList.add("comboselect");
+	this.sourceList.classList.add("source");
+	this.sourceElement.appendChild(this.sourceList);
+	
+	this.targetList = document.createElement("ul");
+	this.targetList.classList.add("comboselect");
+	this.targetList.classList.add("target");
+	this.targetElement.appendChild(this.targetList);
 	
 	this.highlightIndex = null;
 	
@@ -35,217 +40,200 @@ ComboSelect = function(sourceElement, targetElement)
 	this.setDisabled = function(disabled) {
 		_disabled = disabled;
 		if(disabled)
-			this.element.classList.add("disabled");
+		{
+			this.sourceList.classList.add("disabled");
+			this.targetList.classList.add("disabled");
+		}
 		else
-			this.element.classList.remove("disabled");
+		{
+			this.sourceList.classList.remove("disabled");
+			this.targetList.classList.remove("disabled");
+		}
 	};
 	this.isDisabled = function() {
 		return _disabled;
 	};
 	
 	this.unhighlight = function() {
-		for(var i = 0; i < this.sourceElement.children.length; i++)
-			this.sourceElement.children[i].classList.remove("active");
-		for(var i = 0; i < this.targetElement.children.length; i++)
-			this.targetElement.children[i].classList.remove("active");
+		for(var i = 0; i < this.sourceList.children.length; i++)
+			this.sourceList.children[i].classList.remove("active");
+		for(var i = 0; i < this.targetList.children.length; i++)
+			this.targetList.children[i].classList.remove("active");
 	};
 	this.highlight = function(option) {
-		if(!index)
-			index = this.valueIndex;
-		if(index != null && index >= 0 && index < this.options.length)
-			this.options[index].element.classList.add("active");
-		this.highlightIndex = index;
-	};
-	this.open = function() {
-		this.element.classList.add("open");
-		this.isOpen = true;
-		this.isActive = true;
-		SELECT_OPEN = true;
-	};
-	this.close = function(stayActive) {
-		this.element.classList.remove("open");
-		this.isOpen = false;		
-		this.isActive = false;
-		if(stayActive)
-			this.isActive = true;
-		SELECT_OPEN = false;
-		
-		this.unhighlight();
-		this.highlight();
+		if(option != null)
+			option.classList.add("active");
 	};
 	this.getOptionContent = function(option) {
-		var content = new Array();
-		if(option.image)
-		{
-			content.push("<img src='");
-			content.push(option.image);
-			if(option.imageClass)
-			{
-				content.push("' class='");
-				content.push(option.imageClass);
-			}
-			content.push("' alt='");
-			content.push(option.value);
-			content.push("'>");
-		}
-		content.push("<span class='rawValue'>");
-		content.push(option.value);
-		content.push("</span>");
-		content.push(option.title);
-		return content.join("");
+		return option;
 	};
-	this.onselect = function(oldValue, newValue) {};
-	this.select = function(index, skipOnselect) {
-		var oldValue = this.value;
-		if(index != null && index >= 0 && index < this.options.length)
+	this.selectSource = function(indexes, deselectOthers) {
+		for(var i = 0; i < this.sourceList.children.length; i++)
 		{
-			var option = this.options[index];
-			this.value = option.value;
-			this.valueIndex = index;
-			this.display.innerHTML = this.getOptionContent(option);
+			if(indexes.indexOf(i) != -1)
+				this.sourceList.children[i].classList.add("selected");
+			else if(deselectOthers)
+				this.sourceList.children[i].classList.remove("selected");
 		}
-		else
-		{
-			this.value = null;
-			this.valueIndex = null;
-			this.display.innerHTML = "&nbsp;";
-		}
-		this.unhighlight();
-		this.highlight();
-		if(!skipOnselect)
-			this.onselect(oldValue, this.value);
 	};
-	this.selectByValue = function(value, skipOnselect) {
+	this.selectSourceByValue = function(values, deselectOthers) {
 		// look for a matching value
-		for(var o = 0; o < this.options.length; o++)
+		for(var i = 0; i < this.sourceList.children.length; i++)
 		{
-			if(this.options[o].value == value)
+			if(values.indexOf(this.sourceList.children[i].value) != -1)
+				this.sourceList.children[i].classList.add("selected");
+			else if(deselectOthers)
+				this.sourceList.children[i].classList.remove("selected");
+		}
+	};
+	this.selectTarget = function(indexes, deselectOthers) {
+		for(var i = 0; i < this.targetList.children.length; i++)
+		{
+			if(indexes.indexOf(i) != -1)
+				this.targetList.children[i].classList.add("selected");
+			else if(deselectOthers)
+				this.targetList.children[i].classList.remove("selected");
+		}
+	};
+	this.selectTargetByValue = function(values, deselectOthers) {
+		// look for a matching value
+		for(var i = 0; i < this.targetList.children.length; i++)
+		{
+			if(values.indexOf(this.targetList.children[i].value) != -1)
+				this.targetList.children[i].classList.add("selected");
+			else if(deselectOthers)
+				this.targetList.children[i].classList.remove("selected");
+		}
+	};
+	var _apply = function(from, to) {
+		for(var i = 0; i < from.children.length; i++)
+		{
+			if(from.children[i].classList.contains("selected"))
 			{
-				this.select(o, skipOnselect);
-				return;
+				from.children[i].classList.remove("selected");
+				to.appendChild(from.children[i]);
+				i--; // child is immediately removed, hence following indexes are shifted by 1
 			}
 		}
-		this.select(null, skipOnselect);
 	};
-	this.selectPrevious = function() {
-		if(this.valueIndex != null && this.valueIndex > 0)
-			this.select(this.valueIndex-1);
-		else
-			this.select(0);
+	this.applySource = function() {
+		_apply(this.sourceList, this.targetList);
 	};
-	this.selectNext = function() {
-		if(this.valueIndex != null && this.valueIndex < this.options.length-1)
-			this.select(this.valueIndex+1);
-		else
-			this.select(0);
+	this.applyTarget = function() {
+		_apply(this.targetList, this.sourceList);
 	};
-	this.update = function() {
-		while(this.list.children.length > 0)
-			this.list.removeChild(this.list.children[0]);
+	var _filter = function(what, matcher) {
+		for(var i = 0; i < what.children.length; i++)
+		{
+			if(matcher(what.children[i].value))
+				what.children[i].classList.remove(UI.constants.HIDDEN_CLASS);
+			else
+				what.children[i].classList.add(UI.constants.HIDDEN_CLASS);
+		}
+	};
+	this.filterSource = function(matcher) {
+		_filter(this.sourceList);
+	};
+	this.filterTarget = function(matcher) {
+		_filter(this.targetList);
+	};
+	this.addOptions = function(options) {
+		for(var o = 0; o < options.length; o++)
+		{
+			console.log("adding option: " + options[o]);
+			
+			var option = document.createElement("li");
+			option.value = options[o];
+			option.innerHTML = this.getOptionContent(options[o]);
+			
+			var add = document.createElement("span");
+			add.innerHTML = "+";
+			add.classList.add("add");
+			add.onclick = function(comboselect) {
+				return function(event) {
+					this.parentElement.classList.remove("selected");
+					comboselect.targetList.appendChild(this.parentElement);
+				};
+			} (this);
+			option.appendChild(add);
+			
+			var remove = document.createElement("span");
+			remove.innerHTML = "-";
+			remove.classList.add("remove");
+			remove.onclick = function(comboselect) {
+				return function(event) {
+					this.parentElement.classList.remove("selected");
+					comboselect.sourceList.appendChild(this.parentElement);
+				};
+			} (this);
+			option.appendChild(remove);
+			
+			this.sourceList.appendChild(option);
 
-		for(var i = 0; i < this.options.length; i++)
-		{
-			if(this.options[i].element == null)
+			option.onclick = function(comboselect)
 			{
-				this.options[i].element = document.createElement("li");
-				this.options[i].element.innerHTML = this.getOptionContent(this.options[i]);
-				this.list.appendChild(this.options[i].element);
-				
-				this.options[i].element.onclick = function(select, i) {
-					return function(event)
-					{
-						event.preventDefault();
-						select.select(i);
-						select.close(true);// stay active	
-					};
-				} (this, i);
-				this.options[i].element.onmousemove = function(select, i) {
-					return function(event)
-					{
-						select.unhighlight();
-						select.highlight(i);
-					};
-				} (this, i);
-			}
-		}
-	};
-	this.filter = function(matcher) {
-		for(var i = 0; i < this.options.length; i++)
-		{
-			if(matcher(this.options[i].value))
-				this.options[i].element.classList.remove(UI.constants.HIDDEN_CLASS);
-			else
-				this.options[i].element.classList.add(UI.constants.HIDDEN_CLASS);
-		}
-	};
-	this.element.onclick = function(select) {
-		return function(event)
-		{
-			if(Elements.isChildOrSame(select.displayParent, event.target))
-			{
-				if(select.isOpen)
+				return function(event)
 				{
-					select.close(true);
-				}
-				else if(!select.isDisabled())
-				{
-					select.open();
-				}
-				
-			}
-			else
-			{
-				select.close(true);
-			}
-		};
-	} (this);
-	Events.addEventListener(Events.CLICK, function(select) {
-		return function(event) {
-			if(!Elements.isChildOrSame(select.element, event.target))
-				select.close();
-		};
-	} (this));
-	Events.addEventListener(Events.KEYDOWN, function(select) {
-		return function(event) {
-			if(select.isActive && !select.isDisabled())
-			{
-				switch(event.keyCode) {
-					case 37: // left
-					case 38: // up
-						select.selectPrevious();
-						break; 
-					case 39: // right
-					case 40: // down
-						select.selectNext();
-						break;
-					case 33: // page up
-					case 36: // home
-						select.select(0);
-						break;
-					case 34: // page down
-					case 35: // end
-						select.select(selec.options.length-1);
-						break;
-					case 9: // tab
-						select.select(select.highlightIndex);
-						select.close(); // not active
-						break;
-					case 13: // enter
-						select.select(select.highlightIndex);						
-					case 27: // escape
-						select.close(true); // stay active
-						break;
+					if(event.target != this) //not element itselft, maybe add/remove button
+						return;
 					
-					default:
+					if(Events.hasModifiers(event, Events.KEY_CTRL))
+					{
+						if(this.classList.contains("selected"))
+							this.classList.remove("selected");
+						else
+							this.classList.add("selected");
+					}
+					else if(Events.hasModifiers(event, Events.KEY_SHIFT))
+					{
+						// select all from last to this
+						var lastFound, thisFound;
+						for(var i = 0; i <= this.parentElement.children.length; i++)
+						{
+							if(this.parentElement.children[i] == this.parentElement.lastSelected)
+								lastFound = true;
+							else if(this.parentElement.children[i] == this)
+								thisFound = true;
+							
+							if(lastFound || thisFound)
+								this.parentElement.children[i].classList.add("selected");
+							else
+								this.parentElement.children[i].classList.remove("selected");
+							
+							if(lastFound && thisFound)
+								break;
+						}
+					}
+					else
+					{
+						// deselect all others
+						for(var i = 0; i < this.parentElement.children.length; i++)
+						{
+							this.parentElement.children[i].classList.remove("selected");
+						}
+						// select this one
+						this.classList.add("selected");
+					}
+					this.parentElement.lastSelected = this;
 				}
-				event.cancelBubble = true;
-			}
-		};
-	} (this));
-	Events.addEventListener(Events.KEYUP, function(select) {
+			} (this);
+			option.onmousemove = function(comboselect) {
+				return function(event)
+				{
+					comboselect.unhighlight();
+					comboselect.highlight(this);
+				};
+			} (this);
+		}
+	};
+	
+	Events.addEventListener(Events.MOUSEMOVE, function(comboselect) {
 		return function(event) {
-			event.cancelBubble = true;
+			if(Elements.isChildOrSame(comboselect.sourceList, event.target))
+				return;
+			if(Elements.isChildOrSame(comboselect.targetList, event.target))
+				return;
+			comboselect.unhighlight();
 		};
 	} (this));
-	this.select(null);
 };
