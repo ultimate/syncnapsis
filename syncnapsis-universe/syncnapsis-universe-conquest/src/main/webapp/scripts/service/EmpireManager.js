3d02b83658184860259d9b7797e6211af69176dc
ultimate <ultimatej@gmx.net>
2014-11-28 08:41:03 +0100

    added empire loading #109

--- syncnapsis-universe/syncnapsis-universe-conquest/src/main/webapp/scripts/service/EmpireManager.js
+++ syncnapsis-universe/syncnapsis-universe-conquest/src/main/webapp/scripts/service/EmpireManager.js
@@ -15,36 +15,13 @@
 //@requires("GenericManager")
 //@requires("Client")
 
-PlayerManager = function()
-{	
-	this.login = function(username, password)
+EmpireManager = function()
+{
+	this.getAll = function()
 	{
-		// return Player
-		return function(player) { client.uiManager.onLogin(player); };
-	};
-	
-	this.logout = function()
-	{
-		// return boolean
-		return function(success) { client.uiManager.onLogout(success); };
-	};
-	
-	this.register = function(username, email, password, passwordConfirm)
-	{
-		// return Player
-		return function(player) { client.uiManager.onRegister(player); };
-	};
-	
-	this.getCurrent = function()
-	{
-		// return Player
-		return function(player) {
-			if(player != null)
-				client.uiManager.onLogin(player);
-			else
-				client.uiManager.onLogout(true);
-		};
+		// return empire-list
+		return function(empires) { client.uiManager.onEmpiresLoaded(empires); };
 	};
 };
 
-PlayerManager.prototype = new GenericManager();
+EmpireManager.prototype = new GenericManager();
