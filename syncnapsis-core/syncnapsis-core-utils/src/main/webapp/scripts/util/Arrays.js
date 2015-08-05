00e78646da4b0ac1f8f18d7f71c45137e7b6a5df
ultimate <ultimatej@gmx.net>
2015-08-05 18:35:30 +0200

    added binaryIndexOf #322

--- syncnapsis-core/syncnapsis-core-utils/src/main/webapp/scripts/util/Arrays.js
+++ syncnapsis-core/syncnapsis-core-utils/src/main/webapp/scripts/util/Arrays.js
@@ -53,3 +53,38 @@ Array.prototype.insertAt = function(what, index)
 	this.splice(index, 0, what);
 	return this;
 };
+
+// perform a binary search for the given entry
+Array.prototype.binaryIndexOf = function(what, compFunc)
+{
+	'use strict';
+
+	var minIndex = 0;
+	var maxIndex = this.length - 1;
+	var currentIndex;
+	var currentElement;
+	var resultIndex;
+
+	while (minIndex <= maxIndex) {
+		resultIndex = currentIndex = (minIndex + maxIndex) / 2 | 0;
+		currentElement = this[currentIndex];
+
+		if (compFunc ? compFunc(currentElement, what) < 0 : currentElement < what) {
+			minIndex = currentIndex + 1;
+		}
+		else if (compFunc ? compFunc(currentElement, what) > 0 : currentElement > what) {
+			maxIndex = currentIndex - 1;
+		}
+		else {
+			return currentIndex;
+		}
+	}
+
+	return ~currentIndex;
+};
+
+// perform a binary search for the given array for an element with the given id
+Array.prototype.binaryIndexOfId = function(whatId)
+{
+	return this.binaryIndexOf({id: whatId}, function(o1, o2) { return o1.id - o2.id; });
+};
